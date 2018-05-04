9ebea2e885acb88dd3c1e6b8c87f4489dc9b5cf7
jmjuanes <josemijuanes@gmail.com>
2018-05-04 18:00:36 +0200

    Big rewrite

--- scripts/build.js
+++ scripts/build.js
@@ -1,11 +1,9 @@
-let flow = require("tinyflow");
 let fs = require("fs");
 let path = require("path");
 let sass = require("node-sass");
 let autoprefixer = require("autoprefixer");
 let postcss = require("postcss");
 let cleanCSS = require("clean-css");
-let mkdirp = require("mkdirp");
 
 let config = require("./config.js");
 
@@ -13,66 +11,43 @@ let config = require("./config.js");
 let paths = {
     input: "./scss/siimple.scss",
     output: "./dist/siimple.css",
-    outputMin: "./dist/siimple.min.css"
+    minified: "./dist/siimple.min.css"
 };
 
-//Create the output directory 
-flow.task("dist:create", function (done) {
-    return mkdirp("./dist", function (error) {
-        return done(error);
-    });
-});
-
-//Compile the css files
-flow.task("css:compile", ["dist:create"], function (done) {
-    //Read the main scss file 
-    return fs.readFile(paths.input, "utf8", function (error, content) {
-        if (error) {
-            return done(error); 
-        }
-        //Compile the scss file to generate the css
-        let sassOptions = {data: content, includePaths: ["./scss/", "./node_modules/"]}; 
-        return sass.render(sassOptions, function (error, result) {
-            if (error) {
-                return done(error); 
-            }
-            //Prefix the generated css
-            let prefixer = postcss(autoprefixer({ browsers: ["last 3 versions", "IE 9"], cascade: false })); 
-            return prefixer.process(result.css).then(function (output) {
-                //Append the header
-                output.css = config.getHeader() + output.css;
-                //Write the colpiled css file 
-                return fs.writeFile(paths.output, output.css, "utf8", function (error) {
-                    return done(error);
-                });
-            }).catch(function (error) {
-                return done(error);
-            });
-        });
-    });
-});
-
-//Minify output css file 
-flow.task("css:minify", ["dist:create"], function (done) {
-    return fs.readFile(paths.output, "utf8", function (error, content) {
-        if (error) {
-            return done(error); 
-        }
+//Read the main scss file 
+let content = fs.readFileSync(paths.input, "utf8");
+//Compile the scss file to generate the css
+let sassOptions = {data: content, includePaths: ["./scss/", "./node_modules/"]}; 
+sass.render(sassOptions, function (error, result) {
+    if (error) {
+        process.stdout.write(error.message);
+        return process.exit(1);
+    }
+    //Prefix the generated css
+    let prefixer = postcss(autoprefixer({ browsers: ["last 3 versions", "IE 9"], cascade: false })); 
+    let prefixPromise = prefixer.process(result.css);
+    prefixPromise.then(function (output) {
+        //Write the colpiled css file 
+        let outputCss = config.getHeader() + output.css;
+        process.stdout.write("Saving compiled CSS file as '" + paths.output + "'\n");
+        fs.writeFileSync(paths.output, outputCss, "utf8"); 
         //Clean the css file
-        new cleanCSS({compatibility: "*"}).minify(content, function (error, output) {
+        new cleanCSS({compatibility: "*", level: 2}).minify(output.css, function (error, minified) {
             if (error) {
-                return done(error);
+                process.stdout.write(error.message);
+                return process.exit(1);
             }
-            //Append the header 
-            output.styles = config.getHeader() + output.styles;
-            //Write the minified css file
-            return fs.writeFile(paths.outputMin, output.styles, "utf8", function (error) {
-                return done(error);
-            });
+            //Write the minified file
+            let minifiedCss = config.getHeader() + minified.styles;
+            process.stdout.write("Saving minified CSS file as '" + paths.minified + "'\n");
+            fs.writeFileSync(paths.minified, minifiedCss, "utf8");
+            //Process completed
+            process.exit(0);
         });
     });
+    prefixPromise.catch(function (error) {
+        process.stdout.write(error.message);
+        return process.exit(1);
+    });
 });
-
-//Set default tasks 
-flow.defaultTask(["css:compile", "css:minify"]);
 
