7bc0709759f883d29d5ad5710c7faa15370fc1e5
Josemi <josemijuanes@gmail.com>
2019-08-18 14:17:52 +0200

    Init script for creating symlinks to the modules in the packages folder to the node_modules folder

---
+++ scripts/bootstrap.js
@@ -0,0 +1,25 @@
+let fs = require("fs");
+let path = require("path");
+let pkg = require(path.join(process.cwd(), "package.json"));
+
+//Packages folder
+let pkgsPath = path.join(process.cwd(), "packages");
+let outputPath = path.join(process.cwd(), "node_modules", "@siimple");
+//Create the output folder
+fs.mkdirSync(outputPath, {
+    "recursive": true}
+);
+//Read packages directory
+fs.readdirSync(pkgsPath, "utf8").forEach(function (folder) {
+    let folderPath = path.join(pkgsPath, folder);
+    //Check if folder is not a directory
+    if (fs.statSync(folderPath).isDirectory() === false) {
+        return null;
+    }
+    //Read the package.json file
+    let localPkg = require(path.join(folderPath, "package.json"));
+    let name = localPkg.name.replace("@siimple/", "");
+    //Create the symlink in node_modules folder
+    fs.symlinkSync(folderPath, path.join(outputPath, name));
+});
+
