c8c613510a447f3942639acd941a7c91928f6a0f
Josemi <josemijuanes@gmail.com>
2019-08-23 19:10:49 +0200

    Moved index.js from siimple/press repo

---
+++ packages/press/index.js
@@ -0,0 +1,5 @@
+module.exports.build = require("./src/build.js");
+module.exports.resolveConfig = require("./src/resolve.js").resolveConfig;
+module.exports.loadConfig = require("./src/content.js").readConfig;
+module.exports.log = require("./src/logger.js");
+
