5088dfd07b7ae51c48780dee808d4ebb15f36ba1
Josemi <josemijuanes@gmail.com>
2019-08-24 21:34:10 +0200

    Moved card component from siimple/neutrine repo. Fixed imported styles and changed export type

---
+++ packages/neutrine/src/core/card/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import * as helpers from "../../helpers.js";
+
+//Import card styles
+import "@siimple/css/scss/components/card.scss";
+
+//Card base component
+export const Card = function (props) {
+    return helpers.createMergedElement("div", props, "siimple-card");
+};
+
+//Card header component 
+export const CardHeader = function (props) {
+    return helpers.createMergedElement("div", props, "siimple-card-header");
+};
+
+//Card body component 
+export const CardBody = function (props) {
+    return helpers.createMergedElement("div", props, "siimple-card-body");
+};
+
+//Card footer component 
+export const CardFooter = function (props) {
+    return helpers.createMergedElement("div", props, "siimple-card-footer");
+};
+
+//Card link component
+export const CardLink = function (props) {
+    return helpers.createMergedElement("div", props, "siimple-card-link");
+};
+
+//Card title component 
+export const CardTitle = function (props) {
+    return helpers.createMergedElement("div", props, "siimple-card-title");
+};
+
+//Card subtitle component
+export const CardSubtitle = function (props) {
+    return helpers.createMergedElement("div", props, "siimple-card-subtitle");
+};
+
