37f52ee0dcc3c96ce9bd2a2d5684b99d77d81686
Josemi <josemijuanes@gmail.com>
2019-08-24 20:13:33 +0200

    Moved alert core component from siimple/neutrine repo. Fixed helpers and styles import

---
+++ packages/neutrine/src/core/alert/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import * as helpers from "../../helpers.js";
+
+import "@siimple/css/scss/components/alert.scss";
+
+//Alert component
+export function Alert (props) {
+    //Clone the alert props 
+    let newProps = helpers.filterProps(props, ["className", "color"]);
+    //Initialize the class list
+    let classList = ["siimple-alert"];
+    //Check the alert color property
+    if (typeof props.color === "string") {
+        classList.push("siimple-alert--" + props.color.toLowerCase().trim());
+    }
+    //Append the provided class list 
+    newProps.className = helpers.classNames(classList, props.className);
+    return React.createElement("div", newProps, props.children);
+}
+//Alert default props
+Alert.defaultProps = { 
+    "color": "primary" 
+};
+
+//Alert title component 
+export function AlertTitle (props) {
+    return helpers.createMergedElement("div", props, "siimple-alert-title");
+}
+
+//Alert close component
+export function AlertClose (props) {
+    return helpers.createMergedElement("div", props, "siimple-alert-close");
+}
+
