51507f5265e5eb993f02d20bc0eb64cedb2ef57a
Josemi <josemijuanes@gmail.com>
2019-08-25 15:38:21 +0200

    Moved btn component from siimple/neutrine package. Changed export type and minor changes

---
+++ packages/neutrine/src/core/btn/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import * as helpers from "../../helpers.js";
+
+//Import button styles
+import "@siimple/css/scss/elements/btn.scss";
+
+//Button component
+export const Btn = function (props) {
+    //Initialize the button props 
+    let newProps = helpers.filterProps(props, ["className", "color", "disabled", "fluid", "small"]);
+    //Initialize the class names list 
+    let classList = ["siimple-btn"];
+    //Add the button color
+    if (typeof props.color === "string") {
+        classList.push("siimple-btn--" + props.color.toLowerCase().trim());
+    }
+    //Add the button disabled option
+    if (props.disabled === true) {
+        classList.push("siimple-btn--disabled");
+    }
+    //Check the fluid property
+    if (props.fluid === true) {
+        //btn_props.style = { width: '100%', paddingLeft: '0px', paddingRight: '0px' };
+        classList.push("siimple-btn--fluid");
+    }
+    //Check the small property
+    if (props.small === true) {
+        classList.push("siimple-btn--small");
+    }
+    //Append the provided class names
+    newProps.className = helpers.classNames(classList, props.className);
+    //Return the button element
+    return React.createElement("div", newProps, props.children);
+};
+
+//Default properties values
+Btn.defaultProps = { 
+    "color": "primary", 
+    "disabled": false, 
+    "fluid": false,
+    "small": false
+};
+
+//Button groups
+export const BtnGroup = function (props) {
+    return helpers.createMergedElement("div", props, "siimple-btn-group");
+};
+
