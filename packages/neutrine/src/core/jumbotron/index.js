f10e19baa4023f79da826fae53f2d9e2e769a330
Josemi <josemijuanes@gmail.com>
2019-08-26 14:06:01 +0200

    Moved jumbotron component from siimple/neutrine repo

---
+++ packages/neutrine/src/core/jumbotron/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import * as helpers from "../../helpers.js";
+
+//Import jumbotron styles
+import "@siimple/css/scss/layout/jumbotron.scss";
+
+//Jumbotron base component
+export const Jumbotron = function (props) {
+    //Clone the jumbotron props 
+    let newProps = helpers.filterProps(props, ["className", "color", "size"]);
+    //Initialize the jumbotron class names list
+    let classList = ["siimple-jumbotron"];
+    //Check the jumbotron color
+    if (typeof props.color === "string") {
+        classList.push("siimple-jumbotron--" + props.color.toLowerCase());
+    }
+    //Check the jumbotron size property
+    if (typeof props.size === "string") {
+        classList.push("siimple-jumbotron--" + props.size.toLowerCase());
+    }
+    //Generate the jumbotron classname
+    newProps.className = helpers.classNames(classList, props.className);
+    //Return the parent div
+    return React.createElement("div", newProps, props.children);
+};
+
+//Jumbotron default props
+Jumbotron.defaultProps = {
+    "color": null, 
+    "size": null
+};
+
+//Jumbotron title
+export const JumbotronTitle = function (props) {
+    return helpers.createMergedElement("div", props, "siimple-jumbotron-title");
+};
+
+//Jumbotron subtitle 
+export const JumbotronSubtitle = function (props) {
+    return helpers.createMergedElement("div", props, "siimple-jumbotron-subtitle");
+};
+
+//Jumbotron detail component 
+export const JumbotronDetail = function (props) {
+    return helpers.createMergedElement("div", props, "siimple-jumbotron-detail");
+};
+
