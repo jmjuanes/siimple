e6dacec1d02d5d674cbe04ba6c6c5c8c47772518
Josemi <josemijuanes@gmail.com>
2019-08-26 13:22:04 +0200

    Moved textarea component from siimple/neutrine repo

---
+++ packages/neutrine/src/core/textarea/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import * as helpers from "../../helpers.js";
+
+//Import textarea utils
+import "@siimple/css/scss/form/textarea.scss";
+
+//Textarea component 
+export const Textarea = React.forwardRef(function (props, ref) {
+    //Extend the props 
+    let textareaProps = helpers.filterProps(props, ["fluid", "className"]);
+    //Initialize the textarea class list 
+    let classList = ["siimple-textarea"];
+    //Check the fluid attribute
+    if (typeof props.fluid === "boolean" && props.fluid === true) {
+        classList.push("siimple-textarea--fluid");
+    }
+    //Generate the textare className
+    textareaProps.className = helpers.classNames(classList, props.className);
+    //Save the textarea reference
+    textareaProps.ref = ref;
+    //Return the textarea element
+    return React.createElement("textarea", textareaProps, props.children);
+});
+
