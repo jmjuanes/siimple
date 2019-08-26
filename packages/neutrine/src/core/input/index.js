b8a87410dc369a69519e867799af9afbda628188
Josemi <josemijuanes@gmail.com>
2019-08-26 13:06:23 +0200

    Moved input component from siimple/neutrine repo

---
+++ packages/neutrine/src/core/input/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import * as helpers from "../../helpers.js";
+
+//Import input styles
+import "@siimple/css/scss/form/input.scss";
+
+//Input component
+export const Input = React.forwardRef(function (props, ref) {
+    //Clone the input props
+    let inputProps = helpers.filterProps(props, ["fluid", "className"]);
+    //Initialize the input class list
+    let classList = ["siimple-input"];
+    //Check the fluid property
+    if (typeof props.fluid === "boolean" && props.fluid === true) {
+        classList.push("siimple-input--fluid");
+    }
+    //Generate the input class name
+    inputProps.className = helpers.classNames(classList, props.className);
+    //Save the input reference
+    inputProps.ref = ref;
+    //Return the input element
+    return React.createElement("input", inputProps);
+});
+
