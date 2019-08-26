7e8b368300e15e132b18c527ee195d3fabb456cf
Josemi <josemijuanes@gmail.com>
2019-08-26 14:06:19 +0200

    Moved navbar component from siimple/neutrine repo

---
+++ packages/neutrine/src/core/navbar/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import * as helpers from "../../helpers.js";
+
+//Import navbar styles
+import "@siimple/css/scss/layout/navbar.scss";
+
+//Navbar default class
+export const Navbar = function (props) {
+    //Clone the navbar props 
+    let newProps = helpers.filterProps(props, ["className", "color", "size"]);
+    //Initialize the class list
+    let classList = ["siimple-navbar"];
+    //Check the color
+    if (typeof props.color === "string") {
+        classList.push("siimple-navbar--" + props.color.toLowerCase());
+    }
+    //Check the size
+    if (typeof props.size === "string") {
+        classList.push("siimple-navbar--" + props.size.toLowerCase());
+    }
+    //Generate the navbar classname
+    newProps.className = helpers.classNames(classList, props.className);
+    //Render the navbar
+    return React.createElement("div", newProps, props.children);
+};
+
+//Navbar default props
+Navbar.defaultProps = {
+    "color": null, 
+    "size": null
+};
+
+//Navbar title element
+export const NavbarTitle = function (props) {
+    return helpers.createMergedElement("div", props, "siimple-navbar-title");
+};
+
+//Navbar subtitle class
+export const NavbarSubtitle = function (props) {
+    return helpers.createMergedElement("div", props, "siimple-navbar-subtitle");
+};
+
+//Navbar item element
+export const NavbarItem = function (props) {
+    return helpers.createMergedElement("div", props, "siimple-navbar-item");
+};
+
