d675692b69533ea850e0bcf14e50dd2b4ca8f5c6
Josemi <josemijuanes@gmail.com>
2019-08-27 18:13:18 +0200

    Moved scrim component from siimple/neutrine repo

---
+++ packages/neutrine/src/future/scrim/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import * as helpers from "../../helpers.js";
+
+//Import script styles
+import "./style.scss";
+
+//Explort Scrim component
+//A Scrim is a semi-transparent gradient layer that helps Text appear more readable against backgrounds
+export const Scrim = function (props) {
+    return helpers.createMergedElement("div", props, {
+        "className": "neutrine-scrim"
+    });
+}
+
+//Scrim component default props
+Scrim.defaultProps = {};
+
