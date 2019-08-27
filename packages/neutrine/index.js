bf4743f3031382a1df1405650f0d9e54fb2454a8
Josemi <josemijuanes@gmail.com>
2019-08-27 18:51:28 +0200

    Moved from siimple/neutrine repo. Big rewrite

---
+++ packages/neutrine/index.js
@@ -0,0 +1,34 @@
+//Neutrine version
+export {version} from "./src/version.js";
+
+//Import global styles
+import "./src/style.scss";
+
+//Export helpers
+export * from "./src/global.js";
+export * from "./src/helpers.js";
+
+//Export basic components
+export * from "./src/core/index.js";
+export * from "./src/dashboard/index.js";
+export * from "./src/experiments/index.js";
+export * from "./src/future/index.js";
+export * from "./src/icon/index.js";
+
+//Loaders
+export * from "./src/loaders/basic-loader/index.js";
+export * from "./src/loaders/dna-loader/index.js";
+
+//Widgets
+export * from "./src/datatable/index.js";
+export * from "./src/text-editor/index.js";
+
+//Export testing components
+export * from "./src/test/index.js";
+
+//Export utils components
+export * from "./src/utils/choose.js";
+export * from "./src/utils/for-each.js";
+export * from "./src/utils/if.js";
+export * from "./src/utils/renderer.js";
+
