b22b23d1c7bb15b22688e394a935bd7ba0c39d27
Josemi <josemijuanes@gmail.com>
2019-09-04 13:39:15 +0200

    Updated documentation configuration of siimple-css package

--- packages/siimple-css/docs/config.js
+++ packages/siimple-css/docs/config.js
@@ -3,39 +3,32 @@ let pkg = require("../package.json");
 
 module.exports = {
     "source": __dirname,
-    "target": path.join(__dirname, "public"),
+    "target": path.join(process.cwd(), "docs", "public", "css"),
     "theme": "@siimple/press-theme-siimple/config.js",
+    "base": "/css/",
     "site": {
         "name": "css",
         "description": "A minimal CSS toolkit for building flat, clean and responsive web designs",
         "version": pkg.version,
         "license": "MIT License",
-        //Navbar links
-        "navbarLinks": [
-            {"text": "Getting started", "url": "/installation.html"}
+        //Breadcrumb links
+        "breadcrumbLinks": [
+            {"text": "siimple@css", "url": "/css/"}
         ],
+        //Navbar links
+        "navbarLinks": [],
         //Footer links
         "footerLinks": [
             {"text": "Github Repo", "url": "https://github.com/siimple/siimple", "target": "_blank"},
             {"text": "Issues", "url": "https://github.com/siimple/siimple/issues", "target": "_blank"}
         ],
         "repository": "https://github.com/siimple/siimple",
-        "issues": "https://github.com/siimple/siimple/issues"
     },
     //Custom head tags
     "head": [
-        ["link", {"rel": "stylesheet", "href": "/css/siimple-docs.css"}]
+        ["link", {"rel": "stylesheet", "href": "/assets/css/siimple-docs-css.css"}]
     ],
     //Plugins
-    "plugins": [
-        {
-            "plugin": "@siimple/press-plugin-sass",
-            "options": {
-                "includePaths": [path.join(process.cwd(), "node_modules")]
-            },
-            "source": path.join(__dirname, "styles", "index.scss"),
-            "target": "/css/siimple-docs.css"
-        }
-    ]
+    "plugins": []
 };
 
