a07321f0e3f319aef401ed8e52ffcfdc1e6bd780
Josemi <josemijuanes@gmail.com>
2019-09-07 18:26:43 +0200

    Fixed bug in card components

--- packages/neutrine/src/core/card/index.js
+++ packages/neutrine/src/core/card/index.js
@@ -6,36 +6,50 @@ import "@siimple/css/scss/components/card.scss";
 
 //Card base component
 export const Card = function (props) {
-    return helpers.createMergedElement("div", props, "siimple-card");
+    return helpers.createMergedElement("div", props, {
+        "className": "siimple-card"
+    });
 };
 
 //Card header component 
 export const CardHeader = function (props) {
-    return helpers.createMergedElement("div", props, "siimple-card-header");
+    return helpers.createMergedElement("div", props, {
+        "className": "siimple-card-header"
+    });
 };
 
 //Card body component 
 export const CardBody = function (props) {
-    return helpers.createMergedElement("div", props, "siimple-card-body");
+    return helpers.createMergedElement("div", props, {
+        "className": "siimple-card-body"
+    });
 };
 
 //Card footer component 
 export const CardFooter = function (props) {
-    return helpers.createMergedElement("div", props, "siimple-card-footer");
+    return helpers.createMergedElement("div", props, {
+        "className": "siimple-card-footer"
+    });
 };
 
 //Card link component
 export const CardLink = function (props) {
-    return helpers.createMergedElement("div", props, "siimple-card-link");
+    return helpers.createMergedElement("div", props, {
+        "className": "siimple-card-link"
+    });
 };
 
 //Card title component 
 export const CardTitle = function (props) {
-    return helpers.createMergedElement("div", props, "siimple-card-title");
+    return helpers.createMergedElement("div", props, {
+        "className": "siimple-card-title"
+    });
 };
 
 //Card subtitle component
 export const CardSubtitle = function (props) {
-    return helpers.createMergedElement("div", props, "siimple-card-subtitle");
+    return helpers.createMergedElement("div", props, {
+        "className": "siimple-card-subtitle"
+    });
 };
 
