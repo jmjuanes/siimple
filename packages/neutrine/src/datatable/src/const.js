4dcfab647aaabec5b34645693bd1876270ad3abc
Josemi <josemijuanes@gmail.com>
2019-09-13 12:45:59 +0200

    Added cell class-names constants

--- packages/neutrine/src/datatable/src/const.js
+++ packages/neutrine/src/datatable/src/const.js
@@ -1,7 +1,10 @@
 //Contant values of the datatable
 export const DataTableConst = {
     "mainClass": "neutrine-datatable",
+    "tableClass": "neutrine-datatable-table",
     "containerClass": "neutrine-datatable-container",
-    "highlightedClass": "neutrine-datatable-row--highlighted"
+    "highlightedClass": "neutrine-datatable-row--highlighted",
+    "cellClass": "neutrine-datatable-cell",
+    "headerCellClass": "neutrine-datatable-header-cell"
 };
 
