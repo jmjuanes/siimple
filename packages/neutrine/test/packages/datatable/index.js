aec4a1d739704f91f56130a0279159cf0a42bc3f
Josemi <josemijuanes@gmail.com>
2019-09-13 12:48:05 +0200

    Updated datatable test for testing multiple selectable columns

--- packages/neutrine/test/packages/datatable/index.js
+++ packages/neutrine/test/packages/datatable/index.js
@@ -41,6 +41,28 @@ NeutrineUtils.loadJSON("/raw/packages/datatable/data.json", function (data) {
         "render": function (newProps, reference) {
             //Initialize the columns list
             let columns = Object.values({
+                "select1": {
+                    "key": -1,
+                    "visible": true,
+                    "selectable": true,
+                    "selected": function () {
+                        return true;
+                    },
+                    "onBodySelect": function (event, row, index) {
+                        console.log("Select row " + index);
+                    }
+                },
+                "select2": {
+                    "key": -2,
+                    "visible": true,
+                    "selectable": true,
+                    "selected": function () {
+                        return false;
+                    },
+                    "onBodySelect": function (event, row, index) {
+                        console.log("Select row " + index);
+                    }
+                },
                 "name": {
                     "title": "Name",
                     "key": 0,
@@ -90,16 +112,13 @@ NeutrineUtils.loadJSON("/raw/packages/datatable/data.json", function (data) {
                 "striped": newProps.striped,
                 "border": newProps.border,
                 "hover": newProps.hover,
-                "selectable": newProps.selectable,
+                //"selectable": newProps.selectable,
                 "pagination": newProps.pagination,
                 "bodyRowClassName": function (row, index) {
                     return (parseSalary(row[5]) < 100000) ? "siimple-table-row--warning" : "";
                 },
-                "onBodyCellClick": function () {
+                "onBodyClick": function () {
                     console.log("clicked on cell");
-                },
-                "onBodyRowSelect": function () {
-                    console.log("row selected");
                 }
             });
         }
