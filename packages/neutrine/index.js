81a38a4e194deb0e95fa4f914938fd3d9fbab121
Josemi <josemijuanes@gmail.com>
2019-09-16 16:56:56 +0200

    Reorder exports and added widgets export

--- packages/neutrine/index.js
+++ packages/neutrine/index.js
@@ -11,18 +11,18 @@ export * from "./src/ready.js";
 
 //Export basic components
 export * from "./src/core/index.js";
-export * from "./src/dashboard/index.js";
 export * from "./src/experiments/index.js";
 export * from "./src/future/index.js";
 export * from "./src/icon/index.js";
+export * from "./src/widgets/index.js";
 
 //Loaders
 export * from "./src/loaders/basic-loader/index.js";
 export * from "./src/loaders/dna-loader/index.js";
 
-//Widgets
+//Big components
+export * from "./src/dashboard/index.js";
 export * from "./src/datatable/index.js";
-//export * from "./src/text-editor/index.js";
 
 //Export testing components
 export * from "./src/test/index.js";
