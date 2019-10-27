675fcce384cafe2ace8c43000bd1ccf4f506255e
Josemi <josemijuanes@gmail.com>
2019-10-27 12:29:43 +0100

    website: updated website configuration

--- packages/website/config.js
+++ packages/website/config.js
@@ -3,15 +3,23 @@ let pkg = require("./package.json");
 
 module.exports = {
     "source": __dirname,
-    "target": path.join(__dirname, "public"),
+    "target": path.join(__dirname, "www"),
+    "theme": "@siimple/press-theme-siimple/config.js",
     "base": "/",
     "site": {
+        "name": null,
+        "description": null,
+        "version": null,
+        "license": "MIT License",
+        //Navbr links
+        "navbarLinks": [
+            {"text": "Docs", "url": "/docs/index.html"}
+        ],
+        //Footer links
+        "footerLinks": []
     },
     //Custom head tags
     "head": [
-        ["meta", {"name":"viewport", "content": "width=device-width,initial-scale=1"}],
-        ["link", {"rel": "stylesheet", "href": "https://fonts.googleapis.com/css?family=Montserrat:700"}],
-        ["link", {"rel": "stylesheet", "href": "/assets/css/siimple.min.css"}],
         ["link", {"rel": "stylesheet", "href": "/assets/css/siimple-website.css"}]
     ],
     //Plugins
