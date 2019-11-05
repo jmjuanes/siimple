5f06636869db9dea116d84f2406f4742c5f66171
Josemi <josemijuanes@gmail.com>
2019-11-05 20:50:20 +0100

    studio: added textarea option component

---
+++ packages/studio/src/components/option/textarea/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {Textarea} from "@siimple/neutrine";
+
+export class TextareaOption extends React.Component {
+    constructor(props) {
+        super(props);
+        //Referenced elements
+        this.ref = React.createRef();
+        //Bind methods
+        this.getValue = this.getValue.bind(this);
+    }
+    //Get current value
+    getValue() {
+        return this.ref.current.value.trim();
+    }
+    //Render the text element
+    render() {
+        return React.createElement(Textarea, {
+            "type": "text",
+            "fluid": true,
+            "ref": this.ref,
+            "defaultValue": this.props.value
+        });
+    }
+}
+
