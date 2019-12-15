80045c5d146c3fe522ed94bf6a9de03926415753
Josemi <josemijuanes@gmail.com>
2019-12-15 22:52:10 +0100

    studio: init home page component

---
+++ packages/studio/src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+//Export home page component
+export class Home extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    //Render the home page
+    render() {
+        return "Hello to studio";
+    }
+}
+
