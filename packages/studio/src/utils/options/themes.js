f4dfc0076b659da7e7c7387a45a1f804cba40713
Josemi <josemijuanes@gmail.com>
2019-12-15 13:34:08 +0100

    studio: added available themes for the theme option component

---
+++ packages/studio/src/utils/options/themes.js
@@ -0,0 +1,12 @@
+//Export available theme options
+export const themes = {
+    "light": {
+        "label": "Light",
+        "icon": "gear"
+    },
+    "dark": {
+        "label": "Dark",
+        "icon": "gear"
+    }
+};
+
