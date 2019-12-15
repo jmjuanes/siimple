76fa5c0b583b1556298f37bbe876ed10a70629f3
Josemi <josemijuanes@gmail.com>
2019-12-15 13:33:44 +0100

    studio: added available colors for the color option

---
+++ packages/studio/src/utils/options/colors.js
@@ -0,0 +1,22 @@
+//Export colors definitions
+export const colors = {
+    "primary": {
+        "className": "siimple--bg-primary siimple--color-white"
+    },
+    "secondary": {
+        "className": "siimple--bg-secondary siimple--color-white",
+    },
+    "success": {
+        "className": "siimple--bg-success siimple--color-white"
+    },
+    "warning": {
+        "className": "siimple--bg-warning siimple--color-white"
+    },
+    "error": {
+        "className": "siimple--bg-error siimple--color-white"
+    },
+    "dark": {
+        "className": "siimple--bg-dark siimple--color-white"
+    }
+};
+
