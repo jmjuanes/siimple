ec0b9dd691a35c5b5d0e8337ce5138578526235b
Josemi <josemijuanes@gmail.com>
2019-12-21 18:36:25 +0100

    studio: added available icons list

---
+++ packages/studio/src/utils/options/icons.js
@@ -0,0 +1,19 @@
+//Export available icons
+export const icons = [
+    "star",
+    "rocket",
+    "desktop",
+    "laptop",
+    "mobile",
+    "tablet",
+    "lightbulb",
+    "cloud",
+    "heart",
+    "flask",
+    "bell",
+    "shield",
+    "home",
+    "check",
+    "cross"
+];
+
