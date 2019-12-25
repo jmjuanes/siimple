869f4e89c84d2a24df06b12c011f045a6cdd26de
Josemi <josemijuanes@gmail.com>
2019-12-25 23:31:02 +0100

    studio: added url bar to page header

--- packages/studio/src/pages/Editor/Page/Header/index.js
+++ packages/studio/src/pages/Editor/Page/Header/index.js
@@ -10,25 +10,24 @@ import style from "./style.scss";
 export function PageHeader (props) {
     return (
         <div className={style.root}>
-            <div className="siimple--float-left">
-                {/* Page icon */}
-                <Icon icon="file" className={style.icon} />
-                {/* Page title */}
-                <Heading type="h5" className={style.title}>
-                    <span>{props.page.name}</span>
-                </Heading>
+            <div className={style.url}>
+                <Icon className={style.urlIcon} icon="file" />
+                <span className={style.urlText}>
+                    <strong>{props.page.name}</strong> (site:/{props.page.path})
+                </span>
             </div>
-            {/* Page actions */}
-            <div className="siimple--float-right">
-                {/* Page editable icon */}
-                <Renderer render={function () {
-                    let iconImage = (props.editable === true) ? "unlock" : "lock";
-                    return (
-                        <div className={style.button} onClick={props.onEditableClick}>
-                            <Icon icon={iconImage} className={style.buttonIcon} />
-                        </div>
-                    );
-                }} />
+            {/* Page editable icon */}
+            <Renderer render={function () {
+                let iconImage = (props.editable === true) ? "unlock" : "lock";
+                return (
+                    <div className={style.button} onClick={props.onEditableClick}>
+                        <Icon icon={iconImage} className={style.buttonIcon} />
+                    </div>
+                );
+            }} />
+            {/* Page settings */}
+            <div className={style.button}>
+                <Icon icon="gear" className={style.buttonIcon} />
             </div>
         </div>
     );
