3320d41f7370c332453ece2504a17eec35444b70
Josemi <josemijuanes@gmail.com>
2019-12-25 23:57:00 +0100

    studio: fixed bug displaying add block component

--- packages/studio/src/pages/Editor/Page/Content/AddBlock/index.js
+++ packages/studio/src/pages/Editor/Page/Content/AddBlock/index.js
@@ -9,31 +9,33 @@ import style from "./style.scss";
 //Add a new block
 export function AddBlock (props) {
     return (
-        <div className={style.root}>
-            <div className={style.content}>
-                <div className="siimple--mb-3" align="center">
-                    <Heading4 className="siimple--mb-1">
-                        Add new content
-                    </Heading4>
-                    <div style={{"opacity":"0.8"}}>
-                        Add more content blocks to this page.
-                    </div>
-                </div>
-                <ForEach items={getElements()} render={function (key) {
-                    let element = getElementProps(key);
-                    let onClick = function (event) {
-                        return props.onClick(key);
-                    };
-                    //Return the block element
-                    return (
-                        <div className={style.item} align="center" key={key} onClick={onClick}>
-                            <Icon icon={element.icon} style={{"fontSize":"28px"}} /> 
-                            <div className="siimple--mt-1">
-                                <strong>{element.title}</strong>
-                            </div>
+        <div className="siimple--py-2">
+            <div className={style.root}>
+                <div className={style.content}>
+                    <div className="siimple--mb-3" align="center">
+                        <Heading4 className="siimple--mb-1">
+                            Add new content
+                        </Heading4>
+                        <div style={{"opacity":"0.8"}}>
+                            Add more content blocks to this page.
                         </div>
-                    );
-                }} />
+                    </div>
+                    <ForEach items={getElements()} render={function (key) {
+                        let element = getElementProps(key);
+                        let onClick = function (event) {
+                            return props.onClick(key);
+                        };
+                        //Return the block element
+                        return (
+                            <div className={style.item} align="center" key={key} onClick={onClick}>
+                                <Icon icon={element.icon} style={{"fontSize":"28px"}} /> 
+                                <div className="siimple--mt-1">
+                                    <strong>{element.title}</strong>
+                                </div>
+                            </div>
+                        );
+                    }} />
+                </div>
             </div>
         </div>
     );
