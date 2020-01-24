c5d2489c2c5e25d0f875f5fa16910abff8a18d00
Josemi <josemijuanes@gmail.com>
2020-01-24 13:05:25 +0100

    neutrine: added item test

---
+++ packages/neutrine/test/packages/item/index.js
@@ -0,0 +1,33 @@
+Neutrine.ready(function () {
+    let ItemTest = function (props) {
+        return (
+            <div className="siimple--px-5 siimple--py-5">
+                <Neutrine.GridRow>
+                    <Neutrine.GridCol size="4">
+                        <Neutrine.Item>
+                            <Neutrine.ItemBefore>
+                                <Neutrine.ItemIcon icon="rocket" className="siimple--color-dark siimple--bg-light" />
+                            </Neutrine.ItemBefore>
+                            <Neutrine.ItemContent>
+                                <Neutrine.Heading type="h6" className="siimple--mb-0">
+                                    testing.html
+                                </Neutrine.Heading>
+                                <div style={{"fontSize":"13px"}} className="siimple-small">
+                                    Uploaded by <strong>username</strong> · 2Kb
+                                </div>
+                            </Neutrine.ItemContent>
+                            <Neutrine.ItemAfter className="siimple--py-1">
+                                <Neutrine.Icon icon="pen" style={{"fontSize":"20px"}} className="siimple--color-dark" />
+                            </Neutrine.ItemAfter>
+                            <Neutrine.ItemAfter className="siimple--py-1">
+                                <Neutrine.Icon icon="trash" style={{"fontSize":"20px"}} className="siimple--color-dark" />
+                            </Neutrine.ItemAfter>
+                        </Neutrine.Item>
+                    </Neutrine.GridCol>
+                </Neutrine.GridRow>
+            </div>
+        );
+    };
+    ReactDOM.render(<ItemTest />, document.getElementById("root"));
+});
+
