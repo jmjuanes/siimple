ced3f5d2f1b34ac8bf3bd0addfba6959ff5e22bd
Josemi <josemijuanes@gmail.com>
2020-01-24 13:21:47 +0100

    neutrine: export centered component

--- packages/neutrine/src/future/index.js
+++ packages/neutrine/src/future/index.js
@@ -8,4 +8,5 @@ export * from "./side/index.js";
 export * from "./split-panel/index.js";
 export * from "./stepper/index.js";
 export * from "./item/index.js";
+export * from "./centered/index.js";
 
