7c61e64d5c05a08724136de9cf98922893fe9f58
Josemi <josemijuanes@gmail.com>
2020-03-27 19:45:40 +0100

    website: removed theme and added header links in config.js file

--- website/config.js
+++ website/config.js
@@ -4,14 +4,18 @@ let pkg = require("./package.json");
 module.exports = {
     "source": __dirname,
     "target": path.join(__dirname, "www"),
-    "theme": "@siimple/press-theme-siimple/config.js",
     "base": "/",
     "site": {
         "analytics": null
     },
     //Custom head tags
     "head": [
+        ["meta", {"name":"viewport", "content": "width=device-width,initial-scale=1"}],
+        ["link", {"rel": "stylesheet", "href": "https://fonts.googleapis.com/css?family=Montserrat:700"}],
+        ["link", {"rel": "stylesheet", "href": "/assets/css/siimple.min.css"}],
+        //["link", {"rel": "stylesheet", "href": "/assets/analytics.css"}],
         ["link", {"rel": "stylesheet", "href": "/assets/css/style.css"}]
+        //["script", {"type": "text/javascript", "src": "/assets/analytics.js"}]
     ],
     //Plugins
     "plugins": []
