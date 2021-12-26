7c0eaf8aaa70f262df9a348357146b4bd96924eb
Josemi <josemijuanes@gmail.com>
2021-12-26 14:05:08 +0100

    build: update bundle configuration

--- bundle.config.js
+++ bundle.config.js
@@ -6,7 +6,7 @@ const icons = require("./config/icons.json");
 const generateColorsVirtualFile = () => {
     const content = [];
     const colorsList = []; //Colors list
-    Object.keys(colors).forEach((colorName) => {
+    Object.keys(colors).forEach(colorName => {
         return Object.keys(colors[colorName]).forEach((colorIndex) => {
             return colorsList.push({
                 "index": `${colorName}-${colorIndex}`,
@@ -15,7 +15,7 @@ const generateColorsVirtualFile = () => {
         });
     });
     content.push("$colors: (");
-    colorsList.forEach((color) => {
+    colorsList.forEach(color => {
         //const sep = (index === colorsList.length - 1) ? "" : ","; //Separator
         return content.push(`    "${color.index}": ${color.value},`);
     });
@@ -25,13 +25,13 @@ const generateColorsVirtualFile = () => {
 };
 
 //Unicode parser
-const parseUnicode = (value) => value.toString(16).toLowerCase();
+const parseUnicode = value => value.toString(16).toLowerCase();
 
 //Generate icons file
 const generateIconsVirtualFile = () => {
     const content = [];
     content.push("$icons: (");
-    icons.forEach((item) => {
+    icons.forEach(item => {
         return content.push(`    "${item.id}": "${parseUnicode(item.unicode)}",`);
     });
     //content.push(") !default;");
@@ -40,34 +40,68 @@ const generateIconsVirtualFile = () => {
     return content.join("\n");
 };
 
-module.exports = {
-    "name": "siimple",
-    "cwd": __dirname,
-    "output": "index.scss",
-    "entry": [
-        // Auto generated modules
-        "./sass/colors.scss",
-        "./sass/icons.scss",
-        // Core modules
-        "./sass/utils.scss",
-        "./sass/constants.scss",
-        "./sass/sheet.scss",
-        "./sass/naming.scss",
-        "./sass/scales.scss",
-        "./sass/breakpoints.scss",
-        "./sass/variants.scss",
-        "./sass/selectors.scss",
-        "./sass/api.scss",
-        "./sass/theme.scss",
-        // Components
-        "./sass/components/*.scss",
-        "./sass/experiments/*.scss",
-        // Plugins
-        "./sass/plugins/*.scss",
-    ],
-    "resolve": {},
-    "virtualFiles": {
-        "./sass/colors.scss": generateColorsVirtualFile(),
-        "./sass/icons.scss": generateIconsVirtualFile(),
+module.exports = Object.values({
+    "index": {
+        "output": "index.scss",
+        "entry": [
+            "./sass/index.scss",
+        ],
+        "resolve": {
+            "siimple:lib": "./lib.scss",
+            "siimple:plugins": "./plugins.scss",
+        },
     },
-};
+    "colors": {
+        "output": "colors.scss",
+        "entry": [
+            "./colors.scss",
+            "./sass/addons/colors.scss",
+        ],
+        "resolve": {
+            "siimple:lib": "./lib.scss",
+        },
+        "virtualFiles": {
+            "./colors.scss": generateColorsVirtualFile(),
+        },
+    },
+    "icons": {
+        "output": "icons.scss",
+        "entry": [
+            "./icons.scss",
+            "./sass/addons/icons.scss",
+        ],
+        "virtualFiles": {
+            "./icons.scss": generateIconsVirtualFile(),
+        },
+    },
+    "plugins": {
+        "output": "plugins.scss",
+        "entry": [
+            "./sass/components/*.scss",
+            "./sass/experiments/*.scss",
+            "./sass/helpers/*.scss",
+            "./sass/plugins/*.scss",
+        ],
+        "resolve": {
+            "siimple:icons": "./icons.scss",
+            "siimple:colors": "./colors.scss",
+            "siimple:lib": "./lib.scss",
+        },
+    },
+    "lib": {
+        "output": "lib.scss",
+        "entry": [
+            "./sass/utils.scss",
+            "./sass/constants.scss",
+            "./sass/naming.scss",
+            "./sass/scales.scss",
+            "./sass/breakpoints.scss",
+            "./sass/variants.scss",
+            // "./sass/selectors.scss",
+            "./sass/plugins.scss",
+            "./sass/theme.scss",
+            "./sass/sheet.scss",
+        ],
+        "resolve": {},
+    },
+});
