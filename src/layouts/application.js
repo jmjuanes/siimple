b4399b10ed5a8f38061ed4e7d719fe6cd7cb8d6d
Josemi Juanes <jmjuanes@users.noreply.github.com>
2022-01-08 18:07:02 +0100

    Minor fixes in application layout component

--- src/layouts/application.js
+++ src/layouts/application.js
@@ -1,28 +1,15 @@
 import React from "react";
+import kofi from "kofi";
 import {Header} from "../components/Header.js";
-import {classNames} from "../utils/classnames.js";
 
-//Layout base component
+// Layout base component
 const ApplicationLayout = props => {
-    const rootClass = classNames("has-flex has-flex-column has-w-full has-h-screen has-items-stretch", {
+    const rootClass = kofi.classNames({
+        "has-flex has-flex-column has-items-stretch": true,
+        "has-w-full has-h-screen": true,
         "has-bg-coolgray-800": props.theme === "dark",
         "has-bg-coolgray-100": props.theme === "light"
     });
-    //const headingClass = classNames("has-flex has-flex-column has-justify-between has-p-6", {
-    //    "has-bg-white": props.theme === "light",
-    //    "has-bg-dark": props.theme === "dark"
-    //});
-    //const brandClass = classNames({
-    //    "has-text-dark hover:has-text-blue-600": props.theme === "light",
-    //    "has-text-white has-text-opacity-80 hover:has-text-opacity-100": props.theme === "dark",
-    //    "has-cursor-hand": true
-    //});
-    //const buttonClass = classNames("has-flex-inline has-h-full has-items-center", {
-    //    "has-text-coolgray-500 hover:has-text-blue-600": props.theme === "light",
-    //    "has-text-white has-text-opacity-60 hover:has-text-opacity-100": props.theme === "dark",
-    //    "has-cursor-hand": true,
-    //    "has-p-2 has-mt-2": true
-    //});
     return (
         <div className={rootClass}>
             {/* Heading section */}
