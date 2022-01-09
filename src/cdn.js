f6116ab415d3fea1090cb7c826139e9fe83b1776
Josemi Juanes <jmjuanes@users.noreply.github.com>
2022-01-09 02:21:04 +0100

    Fix cdn url generator and minor improvements

--- src/cdn.js
+++ src/cdn.js
@@ -1,23 +1,21 @@
 import kofi from "kofi";
-import {isLocalhost} from "./utils/host.js";
+// import {isLocalhost} from "./utils/host.js";
 
-//CDN Urls
-export const CDN_URL_OLD = "https://cdn.jsdelivr.net/npm/{{package}}@{{version}}/dist/{{file}}";
+// CDN Urls
+export const CDN_URL_OLD = "https://cdn.jsdelivr.net/npm/siimple@{{version}}/{{file}}";
 export const CDN_URL_LATEST = "/static/{{file}}";
 
-//Get CDN path of the specified package
-export function getCDNPath (pkg, version, file) {
+// Get CDN path of the specified package
+export const getCDNPath = (version, file) => {
     //let file = config.siimple["include"][pkg].file; //Get file name to import
     //Check for localhost and latest version --> import from custon CDN folder
     //if (isLocalhost() === true && version === "latest") {
     if (version === "latest") {
         return kofi.format(CDN_URL_LATEST, {"file": file});
     }
-    //Not localhost or latest version ---> import from specified CDN
+    // Not localhost or latest version ---> import from specified CDN
     return kofi.format(CDN_URL_OLD, {
-        "package": pkg,
         "version": version,
-        "file": file
+        "file": file,
     });
-}
-
+};
