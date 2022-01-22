b41ca472617f4c8e193860da7f4838dfbd83fb95
Josemi Juanes <jmjuanes@users.noreply.github.com>
2022-01-22 13:48:56 +0100

    Undo modal changes

--- src/components/ModalExample.js
+++ src/components/ModalExample.js
@@ -2,37 +2,28 @@ import React from "react";
 
 export const ModalExample = () => {
     const [visible, setVisible] = React.useState(false);
-    const style = {
-        display: visible ? "block" : "none",
-    };
     return (
         <React.Fragment>
             <div align="center" className="has-mb-4">
-                <div
-                    className="siimple-btn is-full"
-                    data-role="open-modal"
-                    onClick={() => setVisible(true)}
-                >
+                <div className="siimple-btn is-full" onClick={() => setVisible(true)}>
                     <strong>Display modal</strong>
                 </div>
             </div>
-            <div data-role="modal" className="siimple-scrim" style={style}>
-                <div className="siimple-modal">
-                    <div className="has-flex has-items-center">
-                        <div className="siimple-title is-4 has-mb-0">
-                            <strong>Modal</strong>
+            {visible ? (
+                <div className="siimple-scrim">
+                    <div className="siimple-modal">
+                        <div className="has-flex has-items-center">
+                            <div className="siimple-title is-4 has-mb-0">
+                                <strong>Modal</strong>
+                            </div>
+                            <div className="siimple-close has-ml-auto" onClick={() => setVisible(false)} />
                         </div>
-                        <div
-                            className="siimple-close has-ml-auto"
-                            data-role="close-modal"
-                            onClick={() => setVisible(false)}
-                        />
-                    </div>
-                    <div className="has-pt-6">
-                        Content of the modal
+                        <div className="has-pt-6">
+                            Content of the modal
+                        </div>
                     </div>
                 </div>
-            </div>
+            ) : null}
         </React.Fragment>
     );
 };
