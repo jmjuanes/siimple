b763a091b4fb83393a858cb44152f3b82cf8081e
Josemi Juanes <jmjuanes@users.noreply.github.com>
2022-03-03 00:38:59 +0100

    Simplified api and improved bundle generation (#122)

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,59 @@ const gulp = require("gulp");
 const postcss = require("gulp-postcss");
 const rename = require("gulp-rename");
 
+// Modules
 const bundle = require("./scripts/bundle.js");
 const css = require("./scripts/css.js");
 const icons = require("./scripts/icons.js");
+
+// Configurations
+const colorsList = require("./colors.json");
+const iconsList = require("./icons.json");
 // const pkg = require("./package.json");
+
+// Generate colors file
+const generateColorsFile = () => {
+    const content = [];
+    content.push("$colors: (");
+    Object.keys(colorsList).forEach(name => {
+        Object.keys(colorsList[name]).forEach((index) => {
+            content.push(`    "${name}-${index}": ${colorsList[name][index]},`);
+        });
+    });
+    content.push(");");
+    return content.join("\n");
+};
+
+// Generate icons file
+const generateIconsFile = () => {
+    const content = [];
+    content.push("$icons: (");
+    iconsList.forEach(item => {
+        content.push(`    "${item.id}": "${item.unicode.toString(16).toLocaleLowerCase()}",`);
+    });
+    content.push(");");
+    return content.join("\n");
+};
+
 
 // Clean output directories
 gulp.task("clean", () => null);
 
 // Build bundle files
 gulp.task("build:bundle", () => {
-    return gulp.src("config/bundle.json")
+    return gulp.src([
+        "sass/constants.scss",
+        "sass/utils.scss",
+        "sass/*.scss",
+    ])
         .pipe(bundle({
-            cwd: __dirname,
+            // cwd: __dirname,
+            plugins: [
+                bundle.virtualFilePlugin({
+                    "colors-list.scss": generateColorsFile(),
+                    "icons-list.scss": generateIconsFile(),
+                }),
+            ],
         }))
         .pipe(gulp.dest("dist/"));
 });
@@ -28,7 +68,7 @@ gulp.task("build:components", () => {
 
 // Build icons fonts
 gulp.task("build:icons", () => {
-    return gulp.src("config/icons.json")
+    return gulp.src("icons.json")
         .pipe(icons({
             cwd: __dirname,
             iconsFolder: path.join(__dirname, "icons"),
