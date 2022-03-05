4fee8ea52d4f7f134c945361a867f0dc9d062a57
Josemi <josemijuanes@gmail.com>
2022-03-05 14:26:53 +0100

    Update color palette element

--- website/src/components/ColorPalette.js
+++ website/src/components/ColorPalette.js
@@ -4,19 +4,19 @@ import colors from "siimple-colors";
 
 // Color items
 const ColorItems = props => (
-    <div className="">
+    <div className="columns">
         {Object.keys(props.items).map(item => {
             const itemClassName = kofi.classNames([
                 "has-radius has-p-6 has-mb-2",
                 `has-bg-${props.name}-${item}`,
             ]);
             return (
-                <div key={item} className="has-mb-2">
+                <div key={item} className="column is-one-third is-half-mobile has-pt-0">
                     <div className={itemClassName} />
                     <div className="has-d-flex has-items-center">
                         <div className="has-weight-bold">{item}</div>
-                        <div className="has-ml-auto has-text-small">
-                            {props.items[item]}
+                        <div className="has-ml-auto">
+                            <code className="has-text-coolgray-600">{props.items[item]}</code>
                         </div>
                     </div>
                 </div>
@@ -30,10 +30,10 @@ export const ColorPalette = props => (
     <div className="has-mb-6">
         {Object.keys(colors).map(name => (
             <div className="columns" key={name}>
-                <div className="column is-one-third is-full-mobile">
-                    <strong className="has-text-capitalize">{name}</strong>    
+                <div className="column is-one-quarter is-full-mobile">
+                    <div className="title is-4 has-mt-0 has-text-capitalize">{name}</div>    
                 </div>
-                <div className="column is-two-thirds is-full-mobile">
+                <div className="column is-three-quarters is-full-mobile">
                     <ColorItems name={name} items={colors[name]} />
                 </div>
             </div>
