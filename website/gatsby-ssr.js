222b1c2f8958930d43bbf5489212322955d1006e
Josemi <josemijuanes@gmail.com>
2022-03-08 20:29:35 +0100

    Add extra-bold styles to fallback font import

--- website/gatsby-ssr.js
+++ website/gatsby-ssr.js
@@ -16,7 +16,7 @@ exports.onRenderBody = body => {
         <meta charSet="utf-8" />,
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />,
         //<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" />,
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" />,
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" />,
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@800&display=swap" />,
         <link rel="stylesheet" href="/codecake.css" />,
         <link rel="stylesheet" href="/siimple.min.css" />,
