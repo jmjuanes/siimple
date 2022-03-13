ffaa7c0b3821977b9be9044555cfb1dad2e832e6
Josemi <josemijuanes@gmail.com>
2022-03-13 10:46:46 +0100

    Fix and improve icons cheatsheet

--- website/src/components/IconsCheatsheet.js
+++ website/src/components/IconsCheatsheet.js
@@ -10,10 +10,11 @@ const getIconUnicode = icon => icon.unicode.toString(16).toLowerCase();
 
 // Export Icons Cheatsheet component
 export const IconsCheatsheet = () => (
-    <div className="column">
+    <div className="columns">
         {sortedIcons.map(icon => (
             <div key={icon.id} className="column is-one-third-tablet is-full-mobile has-d-flex has-items-center">
-                <i className={`icon-${icon.id}`} />
+                <i className={`icon-${icon.id} has-text-lg`} />
+                <div className="has-pl-2">{icon.id}</div>
                 <div className="has-ml-auto">
                     <code className="has-text-coolgray-500 has-text-xs">{getIconUnicode(icon)}</code>
                 </div>
