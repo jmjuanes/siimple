da8429cfa24f0765e5956aa33df8afa48c6700f3
Josemi Juanes <jmjuanes@users.noreply.github.com>
2022-03-13 23:36:47 +0100

    Add pagination in icons gallery

--- website/src/components/IconsGallery.js
+++ website/src/components/IconsGallery.js
@@ -23,22 +23,25 @@ const getDisplayedIcons = query => {
     return !query ? sortedIcons : sortedIcons.filter(icon => icon.id.includes(query)); 
 };
 
+// Icons list component
 const IconsList = props => {
     const [page, setPage] = React.useState(0);
     const displayedIcons = getDisplayedIcons(props.query);
+    const totalPages = Math.ceil(displayedIcons.length / props.pagination);
     const visibleIcons = !props.pagination ? displayedIcons : displayedIcons.slice(page, page + props.pageSize);
     return (
         <div>
-            <div className="has-radius has-bg-coolgray-200 has-p-6">
+            <div className="has-radius has-bg-coolgray-200 has-p-6 has-mb-2">
                 {/* Current query */}
-                {kofi.when(!!props.query, () => (
-                    <div className="has-mb-2">
+                <div className="has-mb-2 has-d-inline-flex has-items-center">
+                    <span className="has-mr-4 has-weight-bold">${displayedIcons.length} icons</span>
+                    {kofi.when(!!props.query, () => (
                         <span className="badge has-bg-coolgray-600 has-d-inline-flex has-items-center">
                             <div className="has-size-lg has-pr-2">{props.query}</div>
                             <div className="close" onClick={props.onQueryClear} />
                         </span>
-                    </div>
-                ))}
+                    ))}
+                </div>
                 {/* No icons found message */}
                 {kofi.when(displayedIcons.length === 0, () => (
                     <div align="center" className="has-mx-auto has-w-full has-maxw-128">
@@ -72,9 +75,34 @@ const IconsList = props => {
             </div>
             {/* Display pagination */}
             {kofi.when(props.pagination && displayedIcons.length > 0, () => {
+                const pages = Array.from(Array(totalPages).keys());
+                const firstClass = kofi.classNames({
+                    "has-p-3": true,
+                    "has-cursor-pointer": page > 0,
+                    "has-text-coolgray-400": page === 0,
+                });
+                const lastClass = kofi.classNames({
+                    "has-p-3": true,
+                    "has-cursor-pointer": page < totalPages - 1,
+                    "has-text-coolgray-400": totalPages - 1 <= page,
+                });
                 return (
                     <div className="has-d-flex">
                         <div className="has-ml-auto has-p-2 has-radius has-bg-coolgray-200">
+                            <div className={firstClass} onClick={() => setPage(0)}>First</div>
+                            {pages.map(index => {
+                                const itemClass = kofi.classNames({
+                                    "has-radius has-p-3": true,
+                                    "has-cursor-pointer hover:has-text-blue-500": index !== page,
+                                    "has-bg-white has-text-blue-500": index === page,
+                                });
+                                return (
+                                    <div key={index} className={itemClass} onClick={() => setPage(index)}>
+                                        {(index + 1)}
+                                    </div>
+                                );
+                            })}
+                            <div className={lastClass} onClick={() => setPage(totalPages - 1)}>Last</div>
                         </div>
                     </div>
                 );
@@ -83,6 +111,7 @@ const IconsList = props => {
     );
 };
 
+// Icon modal component
 const IconModal = props => {
     const [iconCopied, setIconCopied] = React.useState(false);
     const previewClass = kofi.classNames([
