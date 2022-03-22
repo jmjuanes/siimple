a7e9f9fcba7df3c08fadce6fc39c181241ba7de0
Josemi Juanes <jmjuanes@users.noreply.github.com>
2022-03-22 21:07:03 +0100

    Add default configuration

---
+++ globals.js
@@ -0,0 +1,79 @@
+// Column sizes
+export const columns = {
+    "one-fifth": "20%",
+    "one-quarter": "25%",
+    "one-third": "33.33%",
+    "two-fifths": "40%",
+    "half": "50%",
+    "three-fifths": "60%",
+    "two-thirds": "66.66%",
+    "three-quarters": "75%",
+    "four-fifths": "80%",
+    "full": "100%",
+};
+
+// Screen sizes
+export const screens = {
+    tablet: "640px",
+    desktop: "1264px",
+    widescreen: "1504px",
+};
+
+// Font family
+export const fonts = {
+    sans: [
+        "-apple-system", 
+        "BlinkMacSystemFont", 
+        "'Segoe UI'", 
+        "Roboto", 
+        "'Helvetica Neue'", 
+        "Arial", 
+        "sans-serif",
+    ],
+    serif: [
+        "Georgia", 
+        "Cambria", 
+        "'Times New Roman'", 
+        "Times", 
+        "serif",
+    ],
+    mono: [
+        "SFMono-Regular", 
+        "Menlo", 
+        "Monaco", 
+        "Consolas", 
+        "'Liberation Mono'", 
+        "'Courier New'", 
+        "monospace",
+    ],
+};
+
+// Heading sizes
+export const headings = {
+    "1": "2.5rem",
+    "2": "2rem",
+    "3": "1.75rem",
+    "4": "1.5rem",
+    "5": "1.25rem",
+    "6": "1rem",
+};
+
+// Global sizes
+export const sizes = {
+    "0": "0px",
+    "1": "0.25rem",
+    "2": "0.5rem",
+    "3": "0.75rem",
+    "4": "1rem",
+    "6": "1.5rem",
+    "8": "2rem",
+    "12": "3rem",
+    "16": "4rem",
+    "24": "6rem",
+    "32": "8rem",
+    "48": "12rem",
+    "64": "16rem",
+    "96": "24rem",
+    "128": "32rem",
+    "256": "64rem",
+};
