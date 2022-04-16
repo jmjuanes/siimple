d5bc50bc2e0199464b07be6c674b630ff03c536b
Josemi <josemijuanes@gmail.com>
2022-04-16 19:38:56 +0200

    Minor improvements

--- website/src/components/IconsCheatsheet.js
+++ website/src/components/IconsCheatsheet.js
@@ -9,7 +9,7 @@ const sortedIcons = icons.sort((a, b) => a.name < b.name ? -1 : +1);
 export const IconsCheatsheet = () => (
     <div className="columns">
         {sortedIcons.map(icon => (
-            <div key={icon.name} className="column is-one-third-tablet is-full-mobile has-d-flex has-items-center">
+            <div key={icon.name} className="column is-one-quarter-tablet is-full-mobile has-d-flex has-items-center">
                 <i className={`icon-${icon.name} has-text-lg`} />
                 <div className="has-ml-auto">
                     <code className="has-text-coolgray-500 has-text-xs">{icon.name}</code>
