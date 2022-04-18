d312e039310e0f12eb5b02a92203d8057b372cf1
Josemi Juanes <jmjuanes@users.noreply.github.com>
2022-04-18 18:07:29 +0200

    Fix minor bug in shadow and font utilities (#143)

--- presets/utilities/index.js
+++ presets/utilities/index.js
@@ -304,7 +304,9 @@ const utilities = [
         states: ["default"],
         responsive: false,
         properties: ["font-family"],
-        values: fonts,
+        values: Object.fromEntries(Object.keys(fonts).map(name => {
+            return [name, fonts[name].join(",")];
+        })),
     },
     {
         shortcut: "text",
@@ -400,9 +402,9 @@ const utilities = [
         responsive: false,
         properties: ["box-shadow"],
         values: {
-            sm: "0 0.25rem 1rem -0.125rem rgba(#000, 0.15),0 0 0 1px rgba(#000, 0.02)",
-            default: "0 0.5rem 1rem -0.25rem rgba(#000, 0.2), 0 0 0 1px rgba(#000, 0.02)",
-            lg: "0rem 1rem 1rem -0.5rem rgba(#000, 0.25),0 0 0 1px rgba(#000, 0.02)",
+            sm: "0 0.25rem 1rem -0.125rem rgba(54,63,79,0.15),0 0 0 1px rgba(54,63,79,0.02)",
+            default: "0 0.5rem 1rem -0.25rem rgba(54,63,79,0.2), 0 0 0 1px rgba(54,63,79,0.02)",
+            lg: "0rem 1rem 1rem -0.5rem rgba(54,63,79,0.25),0 0 0 1px rgba(54,63,79,0.02)",
             none: "none",
         },
     },
