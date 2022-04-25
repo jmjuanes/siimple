fcfa445f3b928ee191890fe139a66bc798624e83
Josemi <josemijuanes@gmail.com>
2022-04-25 19:45:33 +0200

    Fix minor typo in shadow utilities

--- packages/preset-utilities/index.js
+++ packages/preset-utilities/index.js
@@ -59,7 +59,7 @@ const props = {
     shadows: {
         scale: "shadows",
         shortcut: "shadow",
-        properties: ["shadow"],
+        properties: ["boxShadow"],
         states: ["default"],
     },
     // Sizes
