e2802e63db93fde52ae914b804128a91dce65b79
Josemi <josemijuanes@gmail.com>
2022-04-28 20:56:12 +0200

    Update internal configuration

--- siimple.config.js
+++ siimple.config.js
@@ -1,16 +1,32 @@
-import icons from "@siimple/preset-icons";
+import theme from "@siimple/theme";
+import colors from "@siimple/colors";
 import markup from "@siimple/preset-markup";
-import utilities from "@siimple/preset-utilities";
-import reboot from "@siimple/preset-reboot";
+import icons from "@siimple/preset-icons";
 
 export default {
+    useRebootStyles: true,
     useRootStyles: true,
     useBorderBox: true,
     useElements: true,
+    useHelpers: true,
+    prefix: "",
+    // Custom color palette for internal use
+    colors: {
+        ...theme.colors,
+        ...Object.keys(colors).reduce((list, name) => {
+            if (typeof colors[name] === "string") {
+                list[name] = colors[name];
+            }
+            else {
+                Object.keys(colors[name]).forEach(shade => {
+                    list[`${name}-${shade}`] = colors[name][shade];
+                });
+            }
+            return list;
+        }, {}),
+    },
     styles: {
-        ...reboot.styles,
         ...markup.styles,
-        ...utilities.styles,
         ...icons.styles,
     },
 };
