bde7b937997ad91d871a04e4cce4b0a4040a48b9
Josemi Juanes <jmjuanes@users.noreply.github.com>
2022-05-11 00:57:48 +0200

    Forward also core presets

--- siimple/index.js
+++ siimple/index.js
@@ -1,3 +1,12 @@
-// Just export the same API from @siimple/core
-export {css} from "@siimple/core";
-export {mergeConfig, mergeStyles} from "@siimple/core";
+// Export the same API from @siimple/core
+export {css, mergeConfig, mergeStyles} from "@siimple/core";
+
+// Export default theme
+export {default as theme} from "@siimple/preset-theme";
+
+// Export core presets
+export {default as reboot} from "@siimple/preset-reboot";
+export {default as elements} from "@siimple/preset-elements";
+export {default as helpers} from "@siimple/preset-helpers";
+export {default as markup} from "@siimple/preset-markup";
+export {default as icons} from "@siimple/preset-icons";
