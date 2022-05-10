6d7b8b84dd7f9661a9580693655037f948291237
Josemi Juanes <jmjuanes@users.noreply.github.com>
2022-05-11 00:45:30 +0200

    Forward exported modules in entrypoint of siimple package

--- siimple/index.js
+++ siimple/index.js
@@ -1,31 +1,3 @@
-import {buildStyles, mergeConfig, mergeStyles} from "@siimple/core";
-
-// Build siimple
-export default config => {
-    const styles = {};
-    return new Promise(resolve => {
-        // Add borderbox styles
-        if (config.useBorderBox) {
-            mergeStyles(styles, {
-                html: {
-                    boxSizing: "border-box",
-                },
-                "*,*:before,*:after": {
-                    boxSizing: "inherit",
-                }
-            });
-        }
-        // Add root styles
-        if (config.useRootStyles) {
-            mergeStyles(styles, {
-                html: config.root || {},
-            });
-        }
-        // Add custom styles
-        if (config.styles) {
-            mergeStyles(styles, config.styles);
-        }
-        // Build and return css
-        return resolve(buildStyles(styles, config));
-    });
-};
+// Just export the same API from @siimple/core
+export {css} from "@siimple/core";
+export {mergeConfig, mergeStyles} from "@siimple/core";
