b10f4a1aee01cddb89d586bb61b92665349d5ab7
Josemi <josemijuanes@gmail.com>
2022-05-14 23:49:48 +0200

    Update ssr configuration

--- website/gatsby-ssr.js
+++ website/gatsby-ssr.js
@@ -17,7 +17,7 @@ exports.onRenderBody = body => {
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />,
         //<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" />,
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" />,
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@800&display=swap" />,
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;800&display=swap" />,
         <link rel="stylesheet" href="/codecake.css" />,
         <link rel="stylesheet" href="/siimple.css" />,
         <link rel="stylesheet" href="/docs.css" />,
