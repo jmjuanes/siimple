c089164d788fb217d437d8527bcc0e24732c1b1b
Josemi <josemijuanes@gmail.com>
2022-05-14 23:47:30 +0200

    Update section component

--- website/src/components/Section.js
+++ website/src/components/Section.js
@@ -1,6 +1,5 @@
 import React from "react";
 import kofi from "kofi";
-import {Link} from "./Link.js";
 
 const linkClass = kofi.classNames([
     "is-not-underlined is-inline-flex has-items-center", 
@@ -9,25 +8,19 @@ const linkClass = kofi.classNames([
 ]);
 
 export const Section = props => (
-    <div className={props.className}>
+    <div align="center" className={props.className}>
+        <div className="has-mb-3">
+            <strong className="has-text-gray-500 has-size-2">{props.label}</strong>
+        </div>
         <div className="has-mb-2">
-            <span className="badge">{props.label}</span>
-        </div>
-        <div className="title has-size-6 has-weight-black has-mt-4 has-mb-2">{props.title}</div>
-        <div className="has-text-gray-500 has-size-2 has-maxw-half-tablet">
-            {props.children}
-        </div>
-        <div className="has-mt-4">
-            <Link to={props.link} className={linkClass}>
-                <b>Learn more</b> <i className="icon-chevron-right has-ml-2 has-size-0"></i>
-            </Link>
+            <div className="title has-size-8 has-weight-black">{props.title}</div>
         </div>
     </div>
 );
 
 // Default section props
 Section.defaultProps = {
-    className: "has-mb-12 has-mt-12",
+    className: "has-mb-12 has-mt-32",
     title: "",
     label: "Added in v4.0.0",
 };
