baec451a5d4ef360b0680759f26257d3d202d510
Josemi Juanes <jmjuanes@users.noreply.github.com>
2022-06-25 11:34:34 +0000

    Update default config

--- siimple.config.js
+++ siimple.config.js
@@ -1,9 +1,5 @@
 import theme from "@siimple/preset-theme";
 // import colors from "@siimple/colors";
-import reboot from "@siimple/preset-reboot";
-import markup from "@siimple/preset-markup";
-import elements from "@siimple/preset-elements";
-import helpers from "@siimple/preset-helpers";
 import icons from "@siimple/preset-icons";
 
 export default {
@@ -20,10 +16,6 @@ export default {
         "192": "48rem",
     },
     styles: {
-        ...reboot.styles,
-        ...markup.styles,
-        ...elements.styles,
-        ...helpers.styles,
         ...icons.styles,
     },
 };
