8229c338ff183987dc271db35efad5e310cee7c1
Josemi Juanes <jmjuanes@users.noreply.github.com>
2022-06-27 16:36:36 +0000

    Move siimple configuration file to siimple pkg folder

--- siimple/siimple.config.js
+++ siimple/siimple.config.js
@@ -1,21 +1,14 @@
 import theme from "@siimple/preset-theme";
 // import colors from "@siimple/colors";
-import icons from "@siimple/preset-icons";
 
 export default {
     useRootStyles: true,
     useBorderBox: true,
     prefix: "",
     ...theme,
-    fontWeights: {
-        ...theme.fontWeights,
-        black: "900",
-    },
     sizes: {
         ...theme.sizes,
         "192": "48rem",
     },
-    styles: {
-        ...icons.styles,
-    },
+    styles: {},
 };
