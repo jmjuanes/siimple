f8892d2e603498a3ad7c0358a193baeb003e8cea
Josemi Juanes <jmjuanes@users.noreply.github.com>
2022-07-15 01:20:13 +0200

    feat: PostCSS integration (#168)

---
+++ siimple/postcss.cjs
@@ -0,0 +1,42 @@
+// const fs = require("fs");
+const path = require("path");
+const postcss = require("postcss");
+
+const resolveConfig = configOrPath => {
+    if (configOrPath) {
+        // if (typeof configOrPath === "string" && fs.existsSync(configOrPath)) {
+        if (typeof configOrPath === "string") {
+            const configPath = path.resolve(process.cwd(), configOrPath);
+            return import(configPath).then(config => config.default);
+        }
+        else if (typeof configOrPath === "object") {
+            return Promise.resolve(configOrPath);
+        }
+    }
+    return Promise.resolve({}); // No configuration provided
+};
+
+const compileSiimple = config => {
+    const allImports = [
+        import("@siimple/core"),
+        import("@siimple/modules"),
+    ];
+    return Promise.all(allImports).then(imports => {
+        const {css} = imports[0]; // import {css} from "@siimple/core";
+        const {injectModules} = imports[1]; // import {injectModules} from "@siimple/modules";
+        return css(injectModules(config));
+    });
+};
+
+module.exports = configOrPath => ({
+    postcssPlugin: "siimple",
+    plugins: [
+        root => {
+            return resolveConfig(configOrPath)
+                .then(config => compileSiimple(config))
+                .then(styles => root.append(postcss.parse(styles)));
+        },
+    ],
+});
+
+module.exports.postcss = true;
