46cea5771d401bbe708ffc8129d897d25b3a2c29
Josemi Juanes <jmjuanes@users.noreply.github.com>
2022-08-06 13:03:26 +0000

    chore: fix default html code of playground tool

--- website/playground/defaults.js
+++ website/playground/defaults.js
@@ -1,7 +1,7 @@
 // Default code for playground
 export const defaultHtml = `
 <div class="is-flex has-direction-column has-items-center has-justify-center has-w-full has-h-96">
-    <div class="has-w-32 has-h-32 has-bg-gray-800 is-pill">
+    <div class="has-w-32 has-h-32 has-bg-dark is-pill">
         <div class=" has-w-full has-h-full is-flex has-items-center has-justify-center">
             <i class="si-siimple has-size-8 has-text-white"></i>
         </div>
@@ -9,7 +9,7 @@ export const defaultHtml = `
     <div class="has-mt-6 has-size-3 has-w-96 has-pl-8 has-pr-8 has-text-center">
         The <b>minimal</b> and <b>themeable</b> CSS toolkit.
     </div>
-    <div class="has-mt-2 has-text-gray-600">
+    <div class="has-mt-2 is-semitransparent">
         www.siimple.xyz
     </div>
 </div>
