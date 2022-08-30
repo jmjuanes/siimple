0efd0cd42a39a44fc554245effec52286b0ca42d
Josemi Juanes <jmjuanes@users.noreply.github.com>
2022-08-30 18:57:09 +0000

    test: update modules tests

--- test/modules.test.js
+++ test/modules.test.js
@@ -1,4 +1,4 @@
-import {injectModules} from "../packages/modules/index.js";
+import {injectModules, createHelper} from "../packages/modules/index.js";
 
 describe("injectModules", () => {
     it("should exclude specific modules", () => {
@@ -39,5 +39,44 @@ describe("injectModules", () => {
             modules: null,
         });
         expect(Object.keys(config.styles)).toHaveLength(0);
+    });
+});
+
+describe("createHelper", () => {
+    it("should generate helpers from values", () => {
+        const styles = createHelper({
+            prefix: "has",
+            shortcut: "text",
+            properties: ["color"],
+            values: {
+                blue: "blue",
+                white: "white",
+            },
+        });
+        const keys = Object.keys(styles);
+
+        expect(keys).toHaveLength(2);
+        expect(keys[0]).toEqual(".has-text-blue");
+        expect(keys[1]).toEqual(".has-text-white");
+    });
+
+    it("should generate helpers from scale", () => {
+        const config = {
+            fontSizes: ["1", "2", "3"],
+        };
+        const helper = {
+            prefix: "has",
+            shortcut: "size",
+            properties: ["fontSize"],
+            scale: "fontSizes",
+        };
+        const styles = createHelper(helper, config);
+        const keys = Object.keys(styles);
+
+        expect(keys).toHaveLength(3);
+        keys.forEach((key, index) => {
+            expect(key).toEqual(`.has-size-${index}`);
+            expect(styles[key].fontSize?.[0]).toEqual(config.fontSizes[index]);
+        });
     });
 });
