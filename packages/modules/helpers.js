d7182f5ab44544b60b4bd5828f8a17bdcb750534
Josemi Juanes <jmjuanes@users.noreply.github.com>
2022-08-30 18:56:04 +0000

    refactor: improve helpers configuration

--- packages/modules/helpers.js
+++ packages/modules/helpers.js
@@ -1,6 +1,3 @@
-import {createHelper} from "./utils.js";
-
-// Common values
 const colorsValues = {
     white: "#fff",
     black: "#000",
@@ -9,7 +6,7 @@ const colorsValues = {
 };
 const sizesValues = {
     none: "0px",
-    one: "1px",
+    // one: "1px",
     half: "50%",
     full: "100%",
     auto: "auto",
@@ -17,50 +14,34 @@ const sizesValues = {
 
 // Build helpers
 export const helpers = {
-    // Colors
-    backgroundColor: createHelper({
+    backgroundColor: {
         prefix: "has",
         shortcut: "bg",
         properties: ["backgroundColor"],
         states: ["default", "hover"],
-        responsive: false,
         scale: "colors",
-        exclude: ["text", "background", "heading"],
+        excludeFromScale: ["text", "background", "heading"],
         values: colorsValues,
-    }),
-    textColor: createHelper({
+    },
+    textColor: {
         prefix: "has",
         shortcut: "text",
         properties: ["color"],
         states: ["default", "hover"],
-        responsive: false,
         scale: "colors",
-        exclude: ["text", "background", "heading"],
+        excludeFromScale: ["text", "background", "heading"],
         values: colorsValues,
-    }),
-    // Font
-    // font: createHelper({
-    //     prefix: "has",
-    //     shortcut: "font",
-    //     properties: ["fontFamily"],
-    //     states: ["default"],
-    //     responsive: false,
-    //     scale: "fonts",
-    // }),
-    // Font sizes
-    fontSize: createHelper({
+    },
+    fontSize: {
         prefix: "has",
         shortcut: "size",
         properties: ["fontSize"],
-        states: ["default"],
         scale: "fontSizes",
-    }),
-    // Font weights
-    fontWeight: createHelper({
+    },
+    fontWeight: {
         prefix: "has",
         shortcut: "weight",
         properties: ["fontWeight"],
-        states: ["default"],
         values: {
             light: "300",
             normal: "400",
@@ -68,166 +49,134 @@ export const helpers = {
             bold: "700",
             black: "900",
         },
-    }),
-    // Line heights
-    lineHeight: createHelper({
+    },
+    lineHeight: {
         prefix: "has",
         shortcut: "lh",
         properties: ["lineHeight"],
-        states: ["default"],
         values: {
             none: "1",
             tight: "1.25",
             normal: "1.5",
             loose: "2",
         },
-    }),
-    // Sizes
-    width: createHelper({
+    },
+    width: {
         prefix: "has",
         shortcut: "w",
         properties: ["width"],
-        states: ["default"],
         responsive: true,
         scale: "sizes",
         values: {
             ...sizesValues,
             screen: "100vw",
         },
-    }),
-    height: createHelper({
+    },
+    height: {
         prefix: "has",
         shortcut: "h",
         properties: ["height"],
-        states: ["default"],
         responsive: true,
         scale: "sizes",
         values: {
             ...sizesValues,
             screen: "100vh",
         },
-    }),
-    // Position
-    ...Object.fromEntries(["bottom","left","right","top"].map(position => {
-        return [position, createHelper({
-            prefix: "has",
-            shortcut: position,
-            properties: [position],
-            states: ["default"],
-            responsive: true,
-            values: {
-                none: "0px",
-                half: "50%",
-                full: "100%",
-            },
-        })];
-    })),
-    // spacing
-    padding: createHelper({
+    },
+    padding: {
         prefix: "has",
         shortcut: "p",
         properties: ["padding"],
-        states: ["default"],
         responsive: true,
         important: true,
         scale: "space",
         values: sizesValues,
-    }),
-    paddingTop: createHelper({
+    },
+    paddingTop: {
         prefix: "has",
         shortcut: "pt",
         properties: ["padding-top"],
-        states: ["default"],
         responsive: true,
         important: true,
         scale: "space",
         values: sizesValues,
-    }),
-    paddingBottom: createHelper({
+    },
+    paddingBottom: {
         prefix: "has",
         shortcut: "pb",
         properties: ["padding-bottom"],
-        states: ["default"],
         responsive: true,
         important: true,
         scale: "space",
         values: sizesValues,
-    }),
-    paddingLeft: createHelper({
+    },
+    paddingLeft: {
         prefix: "has",
         shortcut: "pl",
         properties: ["padding-left"],
-        states: ["default"],
         responsive: true,
         important: true,
         scale: "space",
         values: sizesValues,
-    }),
-    paddingRight: createHelper({
+    },
+    paddingRight: {
         prefix: "has",
         shortcut: "pr",
         properties: ["padding-right"],
-        states: ["default"],
         responsive: true,
         important: true,
         scale: "space",
         values: sizesValues,
-    }),
-    margin: createHelper({
+    },
+    margin: {
         prefix: "has",
         shortcut: "m",
         properties: ["margin"],
-        states: ["default"],
         responsive: true,
         important: true,
         scale: "space",
         values: sizesValues,
-    }),
-    marginTop: createHelper({
+    },
+    marginTop: {
         prefix: "has",
         shortcut: "mt",
         properties: ["margin-top"],
-        states: ["default"],
         responsive: true,
         important: true,
         scale: "space",
         values: sizesValues,
-    }),
-    marginBottom: createHelper({
+    },
+    marginBottom: {
         prefix: "has",
         shortcut: "mb",
         properties: ["margin-bottom"],
-        states: ["default"],
         responsive: true,
         important: true,
         scale: "space",
         values: sizesValues,
-    }),
-    marginLeft: createHelper({
+    },
+    marginLeft: {
         prefix: "has",
         shortcut: "ml",
         properties: ["margin-left"],
-        states: ["default"],
         responsive: true,
         important: true,
         scale: "space",
         values: sizesValues,
-    }),
-    marginRight: createHelper({
+    },
+    marginRight: {
         prefix: "has",
         shortcut: "mr",
         properties: ["margin-right"],
-        states: ["default"],
         responsive: true,
         important: true,
         scale: "space",
         values: sizesValues,
-    }),
+    },
     // Flexbox
-    flexDirection: createHelper({
+    flexDirection: {
         prefix: "has",
         shortcut: "direction",
-        states: ["default"],
         responsive: true,
         properties: ["flex-direction"],
         values: {
@@ -236,33 +185,30 @@ export const helpers = {
             "row-rev": "row-reverse", 
             "column-rev": "column-reverse",
         },
-    }),
-    flexGrow: createHelper({
+    },
+    flexGrow: {
         prefix: "has",
         shortcut: "flex",
-        states: ["default"],
         responsive: true,
         properties: ["flex-grow"],
         values: {
             "grow": "1",
             "no-grow": "0",
         },
-    }),
-    flexShrink: createHelper({
+    },
+    flexShrink: {
         prefix: "has",
         shortcut: "flex",
-        states: ["default"],
         responsive: true,
         properties: ["flex-shrink"],
         values: {
             "shrink": "1",
             "no-shrink": "0",
         },
-    }),
-    flexWrap: createHelper({
+    },
+    flexWrap: {
         prefix: "has",
         shortcut: "flex",
-        states: ["default"],
         responsive: true,
         properties: ["flex-wrap"],
         values: {
@@ -270,11 +216,10 @@ export const helpers = {
             "wrap-rev": "wrap-reverse",
             "no-wrap": "nowrap",
         },
-    }),
-    alignContent: createHelper({
+    },
+    alignContent: {
         prefix: "has",
         shortcut: "content",
-        states: ["default"],
         responsive: true,
         properties: ["align-content"],
         values: {
@@ -285,11 +230,10 @@ export const helpers = {
             around: "space-around",
             evenly: "space-evenly",
         },
-    }),
-    alignItems: createHelper({
+    },
+    alignItems: {
         prefix: "has",
         shortcut: "items",
-        states: ["default"],
         responsive: true,
         properties: ["align-items"],
         values: {
@@ -299,11 +243,10 @@ export const helpers = {
             stretch: "stretch",
             baseline: "baseline",
         },
-    }),
-    alignSelf: createHelper({
+    },
+    alignSelf: {
         prefix: "has",
         shortcut: "self",
-        states: ["default"],
         responsive: true,
         properties: ["align-self"],
         values: {
@@ -314,11 +257,10 @@ export const helpers = {
             stretch: "stretch",
             baseline: "baseline",
         },
-    }),
-    justifyContent: createHelper({
+    },
+    justifyContent: {
         prefix: "has",
         shortcut: "justify",
-        states: ["default"],
         responsive: true,
         properties: ["justify-content"],
         values: {
@@ -329,11 +271,10 @@ export const helpers = {
             around: "space-around",
             evenly: "space-evenly",
         },
-    }),
-    order: createHelper({
+    },
+    order: {
         prefix: "has",
         shortcut: "order",
-        states: ["default"],
         responsive: true,
         properties: ["order"],
         values: {
@@ -341,53 +282,48 @@ export const helpers = {
             first: "-999",
             last: "999",
         },
-    }),
-    // Text
-    textAlign: createHelper({
-        prefix: "has",
-        shortcut: "text",
-        states: ["default"],
-        responsive: false,
-        properties: ["text-align"],
-        values: {
-            justified: "justify", 
-            left: "left", 
-            center: "center", 
-            right: "right",
+    },
+    textAlign: {
+        styles: {
+            ".has-text-justified": {
+                textAlign: "justify",
+            },
+            ".has-text-left": {
+                textAlign: "left",
+            },
+            ".has-text-center": {
+                textAlign: "center",
+            },
+            ".has-text-right": {
+                textAlign: "right",
+            },
         },
-    }),
-    // Vertical align
-    verticalAlign: createHelper({
-        prefix: "has",
-        shortcut: "align",
-        states: ["default"],
-        responsive: false,
-        properties: ["vertical-align"],
-        values: {
-            "baseline": "baseline",
-            "top": "top",
-            "middle": "middle",
-            "bottom": "bottom",
-            "text-top": "text-top",
-            "text-bottom": "text-bottom",
+    },
+    verticalAlign: {
+        styles: {
+            ".has-align-top": {
+                verticalAlign: "top",
+            },
+            ".has-align-middle": {
+                verticalAlign: "middle",
+            },
+            ".has-align-bottom": {
+                verticalAlign: "bottom",
+            },
         },
-    }),
-    // Cursor
-    cursor: createHelper({
+    },
+    cursor: {
+        styles: {
+            ".is-clickable": {
+                cursor: ["pointer", "!important"],
+            },
+            ".is-not-allowed": {
+                cursor: ["not-allowed", "!important"],
+            },
+        },
+    },
+    display: {
         prefix: "is",
-        states: ["default"],
-        responsive: false,
-        properties: ["cursor"],
-        important: true,
-        values: {
-            "clickable": "pointer",
-            "not-allowed": "not-allowed",
-        },
-    }),
-    // Display
-    display: createHelper({
-        prefix: "is",
-        states: ["default"],
         responsive: true,
         properties: ["display"],
         values: {
@@ -398,121 +334,112 @@ export const helpers = {
             "inline-flex": "inline-flex",
             "hidden": "none",
         },
-    }),
-    // Float
-    float: createHelper({
-        prefix: "is",
-        states: ["default"],
-        responsive: true,
-        properties: ["float"],
-        values: {
-            "aligned-left": "left",
-            "aligned-right": "right",
-        },
-    }),
-    // Overflow
-    overflow: createHelper({
-        prefix: "is",
-        states: ["default"],
-        responsive: true,
-        important: true,
-        properties: ["overflow"],
-        values: {
-            "clipped": "hidden",
-            "scrollable": "auto",
-        },
-    }),
-    // Shadow helpers
-    shadow: createHelper({
-        prefix: "is",
-        states: ["default"],
-        responsive: false,
-        // important: true,
-        properties: ["boxShadow"],
-        values: {
-            "shadowed": "0 0.5rem 1rem -0.25rem rgba(54,63,79,0.2), 0 0 0 1px rgba(54,63,79,0.02)",
-            "shadowless": ["none", "!important"],
-        },
-    }),
-    // Border radius
-    radius: createHelper({
-        prefix: "is",
-        states: ["default"],
-        responsive: false,
-        // important: true,
-        properties: ["radius"],
-        values: {
-            "rounded": "0.5rem",
-            "pill": "999px",
-            "radiusless": ["0px", "!important"],
-        },
-    }),
-    // Position
-    position: createHelper({
-        prefix: "is",
-        states: ["default"],
-        responsive: true,
-        properties: ["position"],
-        values: {
-            "relative": "relative",
-            "absolute": "absolute",
-            "sticky": "sticky",
-            "fixed": "fixed",
-        },
-    }),
-    // Text transform
-    textTransform: createHelper([
-        {
-            prefix: "is",
-            states: ["default"],
-            responsive: false,
-            properties: ["fontStyle"],
-            values: {
-                italic: "italic"
+    },
+    float: {
+        styles: {
+            ".is-aligned-left": {
+                float: "left",
+            },
+            ".is-aligned-right": {
+                float: "right",
             },
         },
-        {
-            prefix: "is",
-            states: ["default"],
-            responsive: false,
-            properties: ["textDecoration"],
-            values: {
-                "underlined": "underline",
-                "not-underlined": ["none", "!important"],
+    },
+    overflow: {
+        styles: {
+            ".is-clipped": {
+                overflow: ["hidden", "!important"],
+            },
+            ".is-scrollable": {
+                overflow: ["auto", "!important"],
             },
         },
-        {
-            prefix: "is",
-            states: ["default"],
-            responsive: false,
-            properties: ["textTransform"],
-            values: {
-                "capitalized": "capitalize",
-                "lowercase": "lowercase",
-                "uppercase": "uppercase",
+    },
+    shadow: {
+        styles: {
+            ".is-shadowed": {
+                boxShadow: [
+                    "0 0.5rem 1rem -0.25rem rgba(54,63,79,0.2),",
+                    "0 0 0 1px rgba(54,63,79,0.02)",
+                ],
+            },
+            ".is-shadowless": {
+                boxShadow: ["none", "!important"],
             },
         },
-    ]),
-    // Opacity helpers
-    opacity: createHelper({
-        prefix: "is",
-        states: ["default"],
-        responsive: false,
-        properties: ["opacity"],
-        values: {
-            "semitransparent": "0.5",
-            "transparent": "0",
+    },
+    radius: {
+        styles: {
+            ".is-rounded": {
+                borderRadius: "0.5rem",
+            },
+            ".is-pill": {
+                borderRadius: "9999px",
+            },
+            ".is-radiusless": {
+                borderRadius: ["0px", "!important"],
+            },
         },
-    }),
-    // Text selection
-    textSelection: createHelper({
-        prefix: "is",
-        states: ["default"],
-        responsive: false,
-        important: true,
-        properties: ["userSelect"],
-        values: {
-            "unselectable": "none",
+    },
+    position: {
+        styles: {
+            ".is-relative": {
+                position: "relative",
+            },
+            ".is-absolute": {
+                position: "absolute",
+            },
+            ".is-sticky": {
+                position: "sticky",
+            },
+            ".is-fixed": {
+                position: "fixed",
+            },
+            ".is-top": {
+                top: "0px",
+            },
+            ".is-bottom": {
+                bottom: "0px",
+            },
         },
-    }),
+    },
+    textTransform: {
+        styles: {
+            ".is-italic": {
+                fontStyle: "italic",
+            },
+            ".is-underlined": {
+                textDecoration: "underline",
+            },
+            ".is-not-underlined": {
+                textDecoration: ["none", "!important"],
+            },
+            ".is-capitalized": {
+                textTransform: "capitalize",
+            },
+            ".is-lowercase": {
+                textTransform: "lowercase",
+            },
+            ".is-uppercase": {
+                textTransform: "uppercase",
+            },
+        },
+    },
+    opacity: {
+        styles: {
+            ".is-semitransparent": {
+                opacity: "0.5",
+            },
+            ".is-transparent": {
+                opacity: "0",
+            },
+        },
+    },
+    textSelection: {
+        styles: {
+            ".is-unselectable": {
+                userSelect: ["none", "!important"],
+            },
+        },
+    },
 };
