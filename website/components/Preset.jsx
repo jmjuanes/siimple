832e762493afd86903ded6837e0b232ca82b546b
Josemi Juanes <jmjuanes@users.noreply.github.com>
2022-09-05 23:22:29 +0000

    fix: fixed presets demo component

--- website/components/Preset.jsx
+++ website/components/Preset.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Helmet from "react-helmet";
-import {create} from "@siimple/css";
+import {ThemeProvider, useTheme, useCss} from "@siimple/react";
+import {BaseStyles, Elements} from "@siimple/components";
 
 const tableData = {
     header: ["Rank", "Name", "Team", "Total Points"],
@@ -13,35 +14,32 @@ const tableData = {
 
 const joinColorNames = (...args) => args.filter(n => !!n).join(".");
 
-const getVariants = obj => Object.keys(obj).filter(selector => /\.is-([\w\-_]+)/.test(selector));
-const getVariantName = selector => selector.match(/\.is-([\w\-_]+)/)[1];
-
-// Theme provider
-const ThemeProvider = props => {
-    const value = React.useRef(null);
-    if (!value.current) {
-        value.current = create(props.theme);
-    }
-    return props.render(value.current);
+const getVariants = obj => {
+    return [...new Set(["primary", "secondary", ...Object.keys(obj || {})])].filter(n => n !== "default");
 };
 
 // Title wrapper
 const Title = props => (
-    <div className="has-mt-16">
-        <div className="title is-2">{props.children}</div>
+    <Elements.title className="is-2">{props.children}</Elements.title>
+);
+
+const Divider = () => (
+    <div className="has-mt-6 has-mb-6">
+        <Elements.divider />
     </div>
 );
 
-const ColorsDemo = props => {
+const ColorsDemo = () => {
+    const theme = useTheme();
     return (
-        <ColorPalette colors={props.theme.colors} />
+        <ColorPalette colors={theme.colors} />
     );
 };
 
 const ColorPalette = ({colors, name}) => (
     <React.Fragment>
         {/* Basic colors */}
-        <div className="title is-4 is-capitalized">{name || "base colors"}</div>
+        <Elements.title className="is-3 is-capitalized">{name || "base colors"}</Elements.title>
         <div className="is-flex has-flex-wrap" style={{gap: "0.5rem"}}>
             {Object.keys(colors).filter(c => typeof colors[c] === "string").map(color => (
                 <div key={color}>
@@ -68,112 +66,91 @@ const ColorPalette = ({colors, name}) => (
     </React.Fragment>
 );
 
-const TypographyDemo = props => (
-    <React.Fragment>
-        <div className="title is-4">Font family</div>
-        {Object.keys(props.theme.fonts).map(key => (
-            <div className="columns" key={key}>
-                <div className="column is-one-quarter is-full-mobile">
-                    <strong className="is-capitalized">{key}</strong>
-                </div>
-                <div
-                    className="column is-three-quarters is-full-mobile"
-                    style={{
-                        fontFamily: props.theme.fonts[key],
-                        fontSize: "2rem",
-                    }}
-                >
-                    {props.theme.fonts[key]}
-                </div>
-            </div>
-        ))}
-        <div className="title is-4">Font sizes</div>
-        <div className="is-flex has-items-end has-flex-wrap" style={{gap: "1rem"}}>
-            {props.theme.fontSizes.map(s => (
-                <span key={s} style={{fontSize: s}}>{s}</span>
-            ))}
-        </div>
-    </React.Fragment>
-);
-
-const ElementDemo = props => {
-    const theme = props.theme[props.field] || {};
-    return ( 
+const TypographyDemo = props => {
+    const theme = useTheme();
+    return (
         <React.Fragment>
-            {["primary", "secondary", ...getVariants(theme)].map(key => {
-                const name = theme[key] ? getVariantName(key) : key;
-                let className = "";
-                if (theme[key] && (key === "primary" || key === "secondary")) {
-                    className = props.css({
-                        backgroundColor: key,
-                        ...theme[key],
-                    });
-                }
-                else if (theme[key]) {
-                    className = props.css(theme[key]);
-                }
-                else if (key === "primary" || key === "secondary") {
-                    className = props.css({
-                        backgroundColor: key,
-                    });
-                }
-                return props.render(name, className);
-            })}
+            <Elements.title className="is-3">Font family</Elements.title>
+            {Object.keys(theme?.fonts || {}).map(key => (
+                <div className="columns" key={key}>
+                    <div className="column is-one-quarter is-full-mobile">
+                        <strong className="is-capitalized">{key}</strong>
+                    </div>
+                    <div
+                        className="column is-three-quarters is-full-mobile"
+                        style={{
+                            fontFamily:theme.fonts[key],
+                            fontSize: "2rem",
+                        }}
+                    >
+                        {theme.fonts[key]}
+                    </div>
+                </div>
+            ))}
+            <Elements.title className="title is-3">Font sizes</Elements.title>
+            <div className="is-flex has-items-end has-flex-wrap" style={{gap: "1rem"}}>
+                {(theme?.fontSizes || []).map(s => (
+                    <span key={s} style={{fontSize: s}}>{s}</span>
+                ))}
+            </div>
         </React.Fragment>
     );
 };
 
-const TableDemo = props => {
-    return (
-        <table className="table is-divided">
-            <thead className="">
-                <tr className="">
-                    {tableData.header.map(cell => (<th key={cell}>{cell}</th>))}
-                </tr>
-            </thead>
-            <tbody>
-                {tableData.body.map((row, index) => (
-                    <tr key={index}>
-                        {row.map(cell => (<td key={cell}>{cell}</td>))}
-                    </tr>
-                ))}
-            </tbody>
-        </table>
+const ElementDemo = props => {
+    const theme = useTheme();
+    return ( 
+        <React.Fragment>
+            {getVariants(theme[props.field]).map(key => props.render(key))}
+        </React.Fragment>
     );
 };
 
-const CardDemo = props => {
-    const buttonClass = props.css({
-        backgroundColor: "primary",
-        "&.is-secondary": {
+const TableDemo = () => (
+    <Elements.table className="is-divided">
+        <thead className="">
+            <tr className="">
+                {tableData.header.map(cell => (<th key={cell}>{cell}</th>))}
+            </tr>
+        </thead>
+        <tbody>
+            {tableData.body.map((row, index) => (
+                <tr key={index}>
+                    {row.map(cell => (<td key={cell}>{cell}</td>))}
+                </tr>
+            ))}
+        </tbody>
+    </Elements.table>
+);
+
+const CardDemo = () => {
+    const backgrounds = {
+        primary: useCss({
+            backgroundColor: "primary",
+        }),
+        secondary: useCss({
             backgroundColor: "secondary",
-        },
-        ...props.theme?.buttons || {},
-    });
+        }),
+    };
     return (
         <div className="columns">
-            {["primary", "secondary"].map(key => {
-                const imageClass = props.css({
-                    backgroundColor: key,
-                });
-                return (
-                    <div key={key} className="column is-half is-full-mobile">
-                        <div className="card is-shadowed">
-                            <div className={`is-rounded has-w-full has-p-12 ${imageClass}`} align="center">
-                                <i className="si-image has-size-9 has-text-white" />
-                            </div>
-                            <div className="title is-5">Heading</div>
-                            <div className="paragraph">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            </div>
-                            <div className={`button ${buttonClass} is-${key} is-full`}>
-                                <strong>Action</strong>
-                            </div>
+            {["primary", "secondary"].map(key => (
+                <div key={key} className="column is-half is-full-mobile">
+                    <Elements.card className="is-shadowed">
+                        <div className={`is-rounded has-w-full has-p-12 ${backgrounds[key]}`} align="center">
+                            <i className="si-image has-size-9 has-text-white" />
                         </div>
-                    </div>
-                );
-            })}
+                        <Elements.title className="is-5">Heading</Elements.title>
+                        <Elements.paragraph>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </Elements.paragraph>
+                        <Elements.button variant={key} className="is-full">
+                            <strong>Action</strong>
+                        </Elements.button>
+                    </Elements.card>
+                </div>
+            ))}
         </div>
     );
 };
@@ -181,7 +158,7 @@ const CardDemo = props => {
 const FormField = props => (
     <div className="columns">
         <div className="column is-one-third">
-            <label className={`label ${props.className}`}>{props.title}</label>
+            <Elements.label>{props.title}</Elements.label>
         </div>
         <div class="column is-two-thids">
             {props.children}
@@ -189,128 +166,121 @@ const FormField = props => (
     </div>
 );
 
-const FormDemo = props => {
-    const labelClass = props.css({
-        ...props.theme.forms?.label || {},
-    });
-    const inputClass = props.css({
-        ...props.theme.forms?.input || {},
-    });
-    const selectClass = props.css({
-        ...props.theme.forms?.select || {},
-    });
-    const textareaClass = props.css({
-        ...props.theme.forms?.textarea || {},
-    });
-    const checkboxClass = props.css({
-        ...props.theme.forms?.checkbox || {},
-    });
+const FormDemo = () => (
+    <React.Fragment>
+        <FormField title="Full name">
+            <Elements.input type="text" placeholder="Liam Wilson" />
+        </FormField>
+        <FormField title="Email">
+            <Elements.input type="email" placeholder="liam.wilson@email.com" />
+        </FormField>
+        <FormField title="How many people are comming?">
+            <Elements.select>
+                <option>From 1 to 5</option>
+                <option>More than 5 people</option>
+            </Elements.select>
+        </FormField>
+        <FormField title="Custom message">
+            <Elements.textarea placeholder="Your message"></Elements.textarea>
+        </FormField>
+        <FormField title="Preferences">
+            <Elements.label>
+                <Elements.checkbox />
+                <span className="has-ml-2">I agree to the terms and conditions.</span>
+            </Elements.label>
+            <Elements.label>
+                <Elements.checkbox />
+                <span className="has-ml-2">Subscribe to our newsletter.</span>
+            </Elements.label>
+        </FormField>
+    </React.Fragment>
+);
+
+const ModalDemo = () => {
+    const [visible, setVisible] = React.useState(false);
     return (
         <React.Fragment>
-            <FormField title="Full name" className={labelClass}>
-                <input type="text" className={`input ${inputClass}`} placeholder="Liam Wilson" />
-            </FormField>
-            <FormField title="Email" className={labelClass}>
-                <input type="email" className={`input ${inputClass}`} placeholder="liam.wilson@email.com" />
-            </FormField>
-            <FormField title="How many people are comming?" className={labelClass}>
-                <select className={`select ${selectClass}`}>
-                    <option>From 1 to 5</option>
-                    <option>More than 5 people</option>
-                </select>
-            </FormField>
-            <FormField title="Custom message" className={labelClass}>
-                <textarea className={`textarea ${textareaClass}`} placeholder="Your message"></textarea>
-            </FormField>
-            <FormField title="Preferences" className={labelClass}>
-                <label className={`label ${labelClass}`}>
-                    <input type="checkbox" className={`checkbox ${checkboxClass}`} />
-                    <span className="has-ml-2">I agree to the terms and conditions.</span>
-                </label>
-                <label className={`label ${labelClass}`}>
-                    <input type="checkbox" className={`checkbox ${checkboxClass}`} />
-                    <span className="has-ml-2">Subscribe to our newsletter.</span>
-                </label>
-            </FormField>
+            <Elements.button onClick={() => setVisible(true)}>Open modal</Elements.button>
+            {visible && (
+                <Elements.scrim>
+                    <Elements.modal>
+                        <div className="is-flex">
+                            <Elements.title className="is-4">Modal title</Elements.title>
+                            <div className="has-ml-auto">
+                                <Elements.close onClick={() => setVisible(false)} />
+                            </div>
+                        </div>
+                        <div className="">Modal content</div>
+                    </Elements.modal>
+                </Elements.scrim>
+            )}
         </React.Fragment>
     );
 };
 
 // preset data is in props.pageContext
 export const Preset = props => (
-    <React.Fragment>
+    <ThemeProvider theme={props.theme}>
         <Helmet>
             {(props.fonts || []).map(fontPath => (
                 <link href={fontPath} rel="stylesheet" />
             ))}
         </Helmet>
-        <ThemeProvider theme={props.theme} render={({css}) => (
-            <div
-                className=""
-                style={{
-                    fontFamily: props.theme.fonts.body,
-                    fontSize: props.theme.fontSizes["1"],
-                }}
-            >
-                {/* Header section */}
-                <div className="title is-1 has-mb-none">{props.name}</div>
-                {/* Colors list */}
-                <Title>Colors</Title>
-                <ColorsDemo theme={props.theme || {}} />
-                <Title>Typography</Title>
-                <TypographyDemo theme={props.theme || {}} /> 
-                <Title>Buttons</Title>
-                <ElementDemo
-                    css={css}
-                    theme={props.theme || {}}
-                    field="buttons"
-                    render={(name, className) => (
-                        <div key={name} className={`button ${className} is-capitalized has-mr-1`}>
-                            <strong>{name}</strong>
+        <BaseStyles>
+            {/* Header section */}
+            <Elements.title className="is-1 has-mb-none">{props.name}</Elements.title>
+            {/* Colors list */}
+            <Title>Colors</Title>
+            <ColorsDemo />
+            <Divider />
+            <Title>Typography</Title>
+            <TypographyDemo /> 
+            <Divider />
+            <Title>Buttons</Title>
+            <ElementDemo
+                field="buttons"
+                render={name => (
+                    <Elements.button key={name} variant={name} className="is-capitalized has-mr-1">
+                        <strong>{name}</strong>
+                    </Elements.button>
+                )}
+            />
+            <Divider />
+            <Title>Alerts</Title>
+            <ElementDemo
+                field="alerts"
+                render={name => (
+                    <Elements.alert key={name} variant={name}>
+                        <div className="">
+                            <span className="is-capitalized">{name}</span> alert
                         </div>
-                    )}
-                />
-                <Title>Alerts</Title>
-                <ElementDemo
-                    css={css}
-                    theme={props.theme || {}}
-                    field="alerts"
-                    render={(name, className) => (
-                        <div key={name} className={`alert ${className}`}>
-                            <div className="">
-                                <span className="is-capitalized">{name}</span> alert
-                            </div>
-                            <div className="close has-ml-auto"></div>
-                        </div>
-                    )}
-                />
-                <Title>Badges</Title>
-                <ElementDemo
-                    css={css}
-                    theme={props.theme || {}}
-                    field="badges"
-                    render={(name, className) => (
-                        <span key={name} className={`badge ${className} is-capitalized has-mr-1`}>
-                            <strong>{name}</strong>
-                        </span>
-                    )}
-                />
-                <Title>Table</Title>
-                <TableDemo
-                    theme={props.theme || {}}
-                    css={css}
-                />
-                <Title>Cards</Title>
-                <CardDemo
-                    css={css}
-                    theme={props.theme || {}}
-                />
-                <Title>Forms</Title>
-                <FormDemo
-                    css={css}
-                    theme={props.theme || {}}
-                />
-            </div>
-        )} />
-    </React.Fragment>
+                        <Elements.close className="has-ml-auto" as="div" />
+                    </Elements.alert>
+                )}
+            />
+            <Divider />
+            <Title>Badges</Title>
+            <ElementDemo
+                field="badges"
+                render={name => (
+                    <Elements.badge key={name} variant={name} className="is-capitalized has-mr-1">
+                        <strong>{name}</strong>
+                    </Elements.badge>
+                )}
+            />
+            <Divider />
+            <Title>Table</Title>
+            <TableDemo />
+            <Divider />
+            <Title>Cards</Title>
+            <CardDemo />
+            <Divider />
+            <Title>Forms</Title>
+            <FormDemo />
+            <Divider />
+            <Title>Modals</Title>
+            <ModalDemo />
+            <Divider />
+        </BaseStyles>
+    </ThemeProvider>
 );
