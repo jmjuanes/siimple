c3306ccb7566b50f94bb6de93c8d075e5342ebf6
Josemi Juanes <jmjuanes@users.noreply.github.com>
2022-09-19 22:59:51 +0000

    fix: reset headings margin-bottom

--- packages/modules/reset.js
+++ packages/modules/reset.js
@@ -1,3 +1,5 @@
+const applySameStyles = (items, styles) => Object.fromEntries(items.map(key => ([key, styles])));
+
 export const reset = {
     // Reset box sizing of all elements
     // *,
@@ -20,25 +22,25 @@ export const reset = {
         "-moz-osx-font-smoothing": "grayscale",
     },
     // Reset margins on lists
-    "ul,ol,dl": {
+    ...applySameStyles(["ul", "ol", "dl"], {
         marginBottom: "1rem",
         marginTop: "0",
-    },
+    }),
     // Reset margins on paragraphs
     p: {
         marginBottom: "1rem",
         marginTop: "0",
     },
     // Fix bold weight (in firefox)
-    "strong,b": {
+    ...applySameStyles(["strong", "b"], {
         fontWeight: "bold",
-    },
+    }),
     // Make images and videos responsive by default
-    "img,video": {
+    ...applySameStyles(["img", "video"], {
         display: "block",
         height: "auto",
         maxWidth: "100%",
-    },
+    }),
     // Remove border from iframe
     iframe: {
         border: "0",
@@ -52,4 +54,9 @@ export const reset = {
     // th {
     //     padding: 0;
     // }
+    // Reset headings
+    ...applySameStyles(["h1", "h2", "h3", "h4", "h5", "h6"], {
+        // marginTop: "0px",
+        marginBottom: "0.5rem",
+    }),
 };
